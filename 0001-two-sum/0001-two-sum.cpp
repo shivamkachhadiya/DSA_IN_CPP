adb381ea79f23a0dc1910ed50461e0024672925c
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2025-07-03 19:26:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& arr, int target) {
-        vector<int> ans;
-        for (int i = 0; i < arr.size(); i++) {
-            int complement = target - arr[i];
-            auto it = find(arr.begin()+i+1, arr.end(), complement);
-            if (it != arr.end()) {
-                int index = it - arr.begin();
-                return {i, index};
-            }
-        }
-        return {};
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
     }
 };
