0e29841706ae6b4daa3c438c0e0d23ebf7208690
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2025-08-02 21:00:07 +0530

    Time: 3 ms (43.09%), Space: 9.4 MB (57.75%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -3,19 +3,25 @@ public:
     bool isIsomorphic(string s, string t) {
         if (s.length() != t.length()) return false;
 
-    unordered_map<char, char> mapST;
-    unordered_set<char> mappedValues;
+    unordered_map<char, char> mapST; // s -> t
+    unordered_map<char, char> mapTS; // t -> s
 
     for (int i = 0; i < s.length(); ++i) {
         char c1 = s[i];
         char c2 = t[i];
 
+        // Check mapping from s -> t
         if (mapST.count(c1)) {
-            if (mapST[c1] != c2) return false;  // Mismatch
+            if (mapST[c1] != c2) return false;
         } else {
-            if (mappedValues.count(c2)) return false;  // Already mapped by some other char
             mapST[c1] = c2;
-            mappedValues.insert(c2);
+        }
+
+        // Check mapping from t -> s
+        if (mapTS.count(c2)) {
+            if (mapTS[c2] != c1) return false;
+        } else {
+            mapTS[c2] = c1;
         }
     }
 
