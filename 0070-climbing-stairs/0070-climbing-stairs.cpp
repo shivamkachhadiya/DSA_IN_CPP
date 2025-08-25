1fa04ddead619c886ed14e597e7d5d48f18cde99
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2025-08-25 18:59:13 +0530

    Time: 0 ms (100%), Space: 8.5 MB (52.58%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int solve(int n, int i, vector<int> &dp) {
+        if (i == n) return 1;   // reached the top
+        if (i > n) return 0;    // overshoot
+        if (dp[i] != -1) return dp[i];  // already computed
+
+        return dp[i] = solve(n, i + 1, dp) + solve(n, i + 2, dp);
+    }
+
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, -1);
+        return solve(n, 0, dp);
+    }
+};
