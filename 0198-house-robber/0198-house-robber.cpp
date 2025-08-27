5555f0e4865e9103b78ea856c94e7b975907c77d
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2025-08-27 13:55:21 +0530

    Time: 0 ms (100%), Space: 10.6 MB (46.61%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int solve(int index, vector<int>& nums,vector<int>&dp) {
+    int solve(int index, vector<int>& nums, vector<int>& dp) {
+
         if (index == 0)
             return nums[index];
         if (index < 0)
@@ -18,8 +19,34 @@ public:
         return dp[index];
     }
     int rob(vector<int>& nums) {
+
+        // recursion + memoization
+
+        // int n = nums.size();
+        // vector<int>dp(n,-1);
+        // return solve(n - 1, nums,dp);
+
+        //--------------------recursion + memoization + tabulation-----------------------
+
         int n = nums.size();
-        vector<int>dp(n,-1);
-        return solve(n - 1, nums,dp);
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return nums[0];
+
+        vector<int> dp(n, 0);
+
+        // base cases
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        // fill dp array iteratively
+        for (int i = 2; i < n; i++) {
+            int pick = nums[i] + dp[i - 2]; // if we rob current house
+            int not_pick = dp[i - 1];       // if we skip current house
+            dp[i] = max(pick, not_pick);
+        }
+
+        return dp[n - 1];
     }
 };
