e079944d67fd2409f7ace9a0f10e718ba2c5ff74
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2025-09-06 14:10:16 +0530

    Time: 11 ms (98.18%), Space: 17.8 MB (84.96%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int minCoins(vector<int>& coins, int amount) {
-    vector<int> dp(amount + 1, INT_MAX);
-    dp[0] = 0; // base case: 0 amount = 0 coins
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0; // base case: 0 amount = 0 coins
 
-    for (int i = 1; i <= amount; i++) {
-        for (int c : coins) {
-            if (i - c >= 0 && dp[i - c] != INT_MAX) {
-                dp[i] = min(dp[i], 1 + dp[i - c]);
+        for (int i = 1; i <= amount; i++) {
+            for (int c : coins) {
+                if (i - c >= 0 && dp[i - c] != INT_MAX) {
+                    dp[i] = min(dp[i], 1 + dp[i - c]);
+                }
             }
         }
+
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
     }
-
-    return dp[amount] == INT_MAX ? -1 : dp[amount];
-}
     int coinChange(vector<int>& coins, int amount) {
-    int ans = minCoins(coins, amount);
-    if (ans == -1) 
-        return -1;
-    else 
-        return ans;
+        int ans = minCoins(coins, amount);
+        if (ans == -1)
+            return -1;
+        else
+            return ans;
 
-    return 0;
+        return 0;
     }
 };
