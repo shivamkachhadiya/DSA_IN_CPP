86b270ab00537a329ec8969490ab007106c28cac
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2025-09-10 18:55:49 +0530

    Time: 46 ms (13.24%), Space: 27.5 MB (52.51%) - LeetHub

---
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solve(string &text1,string &text2,int n1,int n2,int i,int j,vector<vector<int>>&dp){
+        if (i >= n1 || j >= n2) return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(text1[i]==text2[j]){
+            return 1+solve(text1,text2,n1,n2,i+1,j+1,dp);
+        }
+        else{
+            dp[i][j]= max(solve(text1,text2,n1,n2,i+1,j,dp),solve(text1,text2,n1,n2,i,j+1,dp));
+        }
+        return dp[i][j];
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int n1=text1.size();
+        int n2=text2.size();
+        int i=0;
+        int j=0;
+        vector<vector<int>>dp(n1,vector<int>(n2,-1));
+        return solve(text1,text2,n1,n2,i,j,dp);
+    }
+};
