9122c266d69676edb8fc1609ab128e95f48e5088
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2025-09-12 16:13:50 +0530

    Time: 575 ms (12.68%), Space: 296.3 MB (34.01%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int solve(int n, vector<int>& a, int curr, int prev, vector<vector<int>>& dp) {
+        if (curr == n) {
+            return 0;
+        }
+        if (dp[curr][prev + 1] != -1) return dp[curr][prev + 1];
+
+        // include
+        int take = 0;
+        if (prev == -1 || a[curr] > a[prev]) {
+            take = 1 + solve(n, a, curr + 1, curr, dp);
+        }
+
+        // exclude
+        int nottake = solve(n, a, curr + 1, prev, dp);
+
+        dp[curr][prev + 1] = max(take, nottake);
+        return dp[curr][prev + 1];
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n + 1, -1));  // ✅ fix: n+1 cols
+        return solve(n, nums, 0, -1, dp);
+    }
+};
