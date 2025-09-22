5dfa8f52cd16d337230f99ee581bef491a9b56b0
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2025-09-22 15:31:35 +0530

    Time: 2715 ms (8.12%), Space: 240.5 MB (60.12%) - LeetHub

--- 1087-longest-arithmetic-subsequence/1087-longest-arithmetic-subsequence.cpp
+++ 1087-longest-arithmetic-subsequence/1087-longest-arithmetic-subsequence.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
-    int solve(int index, int diff, vector<int>& nums,unordered_map<int,int>dp[]) {
+    int solve(int index, int diff, vector<int>& nums) {
         // backword check
         if (index < 0)
             return 0;
-        if(dp[index].count(diff))return dp[index][diff];
         int ans = 0;
         for (int j = index - 1; j >= 0; j--) {
             if (nums[index] - nums[j] == diff) {
-                ans = max(ans, 1 + solve(j, diff, nums,dp));
+                ans = max(ans, 1 + solve(j, diff, nums));
             }
         }
-        return dp[index][diff]=ans;
+        return ans;
+    }
+
+    int solveMem(int index, int diff, vector<int>& nums,
+              unordered_map<int, int> dp[]) {
+        // backword check
+        if (index < 0)
+            return 0;
+        if (dp[index].count(diff))
+            return dp[index][diff];
+        int ans = 0;
+        for (int j = index - 1; j >= 0; j--) {
+            if (nums[index] - nums[j] == diff) {
+                ans = max(ans, 1 + solveMem(j, diff, nums, dp));
+            }
+        }
+        return dp[index][diff] = ans;
     }
     int longestArithSeqLength(vector<int>& nums) {
         int n = nums.size();
@@ -22,7 +37,7 @@ public:
         unordered_map<int,int>dp[n+1];
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                ans = max(ans, 2 + solve(i, nums[j] - nums[i], nums,dp));
+                ans = max(ans, 2 + solveMem(i, nums[j] - nums[i], nums,dp));
             }
         }
 
