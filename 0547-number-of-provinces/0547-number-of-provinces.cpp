685bc143691e9d00245df2724a651cb111dc53ce
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2025-09-26 17:24:13 +0530

    Time: 2 ms (47.25%), Space: 19.3 MB (92.94%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
     void dfs(int node, vector<vector<int>>& isConnected, vector<int>& visited) {
+        //jese hi new node visit kro mark in vector
+
         visited[node] = 1;
+        
+        //ye for loop each row ke liye chalegi
+        
         for (int j = 0; j < isConnected.size(); j++) {
+            //iska matalab node connected hona chahiye
+            // but usko vector mei visit mark na kiya ho matalb vo visted nahi hei
+            //visit krne ke liye dfs call lagaoo
             if (isConnected[node][j] == 1 && !visited[j]) {
                 dfs(j, isConnected, visited);
             }
@@ -13,6 +21,9 @@ public:
         int n = isConnected.size();
         vector<int> visited(n + 1, 0);
         int count = 0;
+
+        //ye main outer loop hei.. jo 0 se mat.size tak jayegi
+        //jo vector mei mark rakhegi and count karegi
         for (int i = 0; i < n; i++) {
             if (visited[i] == 0) {
                 count++;
