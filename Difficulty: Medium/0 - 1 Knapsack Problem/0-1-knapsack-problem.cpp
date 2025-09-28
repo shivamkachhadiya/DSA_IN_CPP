9eb42df314b60ccb835628b85ef1142c160c64b7
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2025-09-28 18:51:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,30 +1,58 @@
 class Solution {
   public:
-  int solve(vector<int>&weight,vector<int>&value,int index,int capacity,vector<vector<int>>&dp){
-      if(index==0){
-          if(weight[0]<=capacity){
-              return value[0];
-          }else{
-              return 0;
-          }
-      }
-      if(dp[index][capacity]!=-1){
-          return dp[index][capacity];
-      }
-      int include=0;
-      if(weight[index]<=capacity){
-          include=value[index]+solve(weight,value,index-1,capacity-weight[index],dp);
-          
-      }
-      int exclude=0+solve(weight,value,index-1,capacity,dp);
-      dp[index][capacity]=max(include,exclude);
-      return dp[index][capacity];
-  }
-    int knapsack(int maxweight, vector<int> &val, vector<int> &weight) {
+    int solve(vector<int>&weight,vector<int>&value,int index,int capacity){
+        if(index==0){
+            if(weight[0]<=capacity){
+                return value[0];
+            }else{
+                return 0;
+            }
+        }
+        
+        //include
+        int take=0;
+        if(weight[index]<=capacity){
+            take=value[index]+solve(weight,value,index-1,capacity-weight[index]);
+        }
+        
+        
+        //exclude
+        int NotTake=0+solve(weight,value,index-1,capacity);
+        
+        return max(take,NotTake);
+    }
+    
+    int solveMEM(vector<int>&weight,vector<int>&value,int index,int capacity,
+                vector<vector<int>>&dp){
+        if(index==0){
+            if(weight[0]<=capacity){
+                return value[0];
+            }else{
+                return 0;
+            }
+        }
+        
+        if(dp[index][capacity]!=-1)return dp[index][capacity];
+        
+        
+        //include
+        int take=0;
+        if(weight[index]<=capacity){
+            take=value[index]+solveMEM(weight,value,index-1,capacity-weight[index],dp);
+        }
+        
+        
+        //exclude
+        int NotTake=0+solveMEM(weight,value,index-1,capacity,dp);
+        
+        return dp[index][capacity]=max(take,NotTake);
+    }
+    
+    
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
         // code here
         int n=val.size();
-        vector<vector<int>>dp(n,vector<int>(maxweight+1,-1));
-        return solve(weight,val,n-1,maxweight,dp);
-        
+        vector<vector<int>>dp(n,vector<int>(W+1,-1));
+        return solveMEM(wt,val,n-1,W,dp);
     }
 };
