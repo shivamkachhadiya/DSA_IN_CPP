79338d9e034b0f938d12c103947c8b9ae8a31597
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2025-10-14 18:07:10 +0530

    Time: 24 ms (7.96%), Space: 40.3 MB (75.22%) - LeetHub

---
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<bool> inc(n, false);
+
+        // Check each subarray of length k
+        for (int i = 0; i + k <= n; i++) {
+            bool ok = true;
+            for (int j = i + 1; j < i + k; j++) {
+                if (nums[j] <= nums[j - 1]) {
+                    ok = false;
+                    break;
+                }
+            }
+            inc[i] = ok;
+        }
+
+        // Check for two adjacent increasing subarrays
+        for (int i = 0; i + 2 * k <= n; i++) {
+            if (inc[i] && inc[i + k])
+                return true;
+        }
+
+        return false;
+    }
+};
