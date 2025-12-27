44597f6bb191f42f36339495ac15cf230247a301
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2025-12-27 12:48:59 +0530

    Time: 562 ms (13.55%), Space: 296.6 MB (11.35%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,52 +1,30 @@
 class Solution {
 public:
-    int solve(int n, vector<int>& a, int curr, int prev,
-              vector<vector<int>>& dp) {
-        if (curr == n) {
-            return 0;
-        }
-        if (dp[curr][prev + 1] != -1)
-            return dp[curr][prev + 1];
-        // include
-        int take = 0;
-        if (prev == -1 || a[curr] > a[prev]) {
-            take = 1 + solve(n, a, curr + 1, curr, dp);
-        }
+    int solveMem(int ind, int prev_ind, vector<int>& arr, int n,
+                 vector<vector<int>>& dp) {
 
-        // exclude
-        int nottake = 0 + solve(n, a, curr + 1, prev, dp);
-
-        dp[curr][prev + 1] = max(take, nottake);
-        return dp[curr][prev + 1];
-    }
-
-    // Optimal LIS using Binary Search + Greedy
-    int solveOptimal(int n, vector<int>&a) {
-        if (n == 0)
+        // Base case
+        if (ind == n)
             return 0;
 
-        vector<int> ans;
-        ans.push_back(a[0]);
+        // Memoization check
+        if (dp[ind][prev_ind + 1] != -1)
+            return dp[ind][prev_ind + 1];
 
-        for (int i = 1; i < n; i++) {
-            if (a[i] > ans.back()) {
-                // If current element is greater than last element in ans,
-                // append it
-                ans.push_back(a[i]);
-            } else {
-                // Find index of the smallest element >= a[i] and replace it
-                int index =
-                    lower_bound(ans.begin(), ans.end(), a[i]) - ans.begin();
-                ans[index] = a[i];
-            }
+        // Not take
+        int len = solveMem(ind + 1, prev_ind, arr, n, dp);
+
+        // Take
+        if (prev_ind == -1 || arr[ind] > arr[prev_ind]) {
+            len = max(len, 1 + solveMem(ind + 1, ind, arr, n, dp));
         }
-        return ans.size(); // length of LIS
-    }
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        // vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        // return solve(n,nums,0,-1,dp);
 
-        return solveOptimal(n, nums);
+        return dp[ind][prev_ind + 1] = len;
+    }
+
+    int lengthOfLIS(vector<int>& arr) {
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        return solveMem(0, -1, arr, n, dp);
     }
 };
