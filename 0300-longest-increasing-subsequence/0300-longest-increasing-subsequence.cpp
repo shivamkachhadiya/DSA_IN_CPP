5a0ac04e0fdd72f5a473eedfee57205e59cd446a
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2025-12-27 13:36:42 +0530

    Time: 392 ms (25.16%), Space: 576.4 MB (5.89%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,30 +1,47 @@
 class Solution {
 public:
-    int solveMem(int ind, int prev_ind, vector<int>& arr, int n,
+    int solveMem(int curr, int prev, vector<int>& arr, int n,
                  vector<vector<int>>& dp) {
 
         // Base case
-        if (ind == n)
+        if (curr == n)
             return 0;
 
         // Memoization check
-        if (dp[ind][prev_ind + 1] != -1)
-            return dp[ind][prev_ind + 1];
+        if (dp[curr][prev + 1] != -1)
+            return dp[curr][prev + 1];
 
         // Not take
-        int len = solveMem(ind + 1, prev_ind, arr, n, dp);
+        int len = solveMem(curr + 1, prev, arr, n, dp);
 
         // Take
-        if (prev_ind == -1 || arr[ind] > arr[prev_ind]) {
-            len = max(len, 1 + solveMem(ind + 1, ind, arr, n, dp));
+        if (prev == -1 || arr[curr] > arr[prev]) {
+            len = max(len, 1 + solveMem(curr + 1, curr, arr, n, dp));
         }
 
-        return dp[ind][prev_ind + 1] = len;
+        return dp[curr][prev + 1] = len;
     }
 
+    int solveTab(vector<int>&arr,int n){
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+
+        for(int curr=n-1;curr>=0;curr--){
+            for(int prev=curr-1;prev>=-1;prev--){
+                int take=0;
+                if(prev==-1 || arr[curr]>arr[prev]){
+                    take=1+dp[curr+1][curr+1];
+                }
+                int notTake=0+dp[curr+1][prev+1];
+
+                dp[curr][prev+1]=max(take,notTake);
+            }
+        }
+        return dp[0][0];
+    }
     int lengthOfLIS(vector<int>& arr) {
         int n = arr.size();
         vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return solveMem(0, -1, arr, n, dp);
+        return solveTab(arr,n);
+        //return solveMem(0, -1, arr, n, dp);
     }
 };
