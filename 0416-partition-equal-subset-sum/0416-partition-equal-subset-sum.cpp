06d2e13c83fd76ce2369f5747d54cdea7e7e1da3
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2025-12-28 16:59:06 +0530

    Time: 179 ms (18.01%), Space: 17.4 MB (58.04%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -47,41 +47,47 @@ public:
     bool solveTab(vector<int>& arr, int n, int index, int target, int sum,
                   vector<vector<bool>>& dp) {
         // base case
+        // base case: sum 0 always possible
         for (int i = 0; i <= n; i++)
             dp[i][0] = true;
 
         // fill table in recursion-like order
-        for (int i = 1; i <= n; i++) {
-            for (int s = 1; s <= target; s++) {
-                bool nottake = dp[i - 1][s];
-                bool take = false;
-                if (s >= arr[i - 1])
-                    take = dp[i - 1][s - arr[i - 1]];
-                dp[i][s] = take || nottake;
+        for (int index = n - 1; index >= 0;
+             index--) {                         // recursion index 0 → n-1
+            for (int t = 0; t <= target; t++) { // recursion sum 0 → target
+                bool incl = false;
+                if (t - arr[index] >= 0)
+                    incl =
+                        dp[index + 1][t - arr[index]]; // take current element
+
+                bool excl = dp[index + 1][t]; // do not take current element
+
+                dp[index][t] = incl || excl;
             }
         }
 
-        return dp[n][target];
-    }
+        // answer starts from index 0, sum 0
+        return dp[0][target];
+                  }
 
-    bool canPartition(vector<int>& arr) {
-        int n = arr.size();
-        int target = 0;
-        for (auto x : arr) {
-            target = target + x;
+        bool canPartition(vector<int> & arr) {
+            int n = arr.size();
+            int target = 0;
+            for (auto x : arr) {
+                target = target + x;
+            }
+
+            if (target % 2 == 0) {
+                target = target / 2;
+                // vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+
+                // return solve(arr, n, 0, target, 0);
+                // return solveMem(arr, n, 0, target, 0, dp);
+                vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
+
+                return solveTab(arr, n, 0, target, 0, dp);
+            } else {
+                return false;
+            }
         }
-
-        if (target % 2 == 0) {
-            target = target / 2;
-            // vector<vector<int>> dp(n, vector<int>(target + 1, -1));
-
-            // return solve(arr, n, 0, target, 0);
-            // return solveMem(arr, n, 0, target, 0, dp);
-            vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
-
-            return solveTab(arr, n, 0, target, 0, dp);
-        } else {
-            return false;
-        }
-    }
-};
+    };
