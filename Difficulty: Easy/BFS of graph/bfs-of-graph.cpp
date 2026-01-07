c92189854055f10ab19e3357a543bf4c6fdcd4ae
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-01-07 22:38:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
@@ -1,41 +1,33 @@
-#include<unordered_map>
-#include<queue>
-#include<list>
 class Solution {
   public:
-  void bfs(vector<vector<int>> &adj,
-        unordered_map<int,bool>&visited,
-            vector<int>&ans,int node){
-                queue<int>q;
-                q.push(node);
-                visited[node]=1;
-                while(!q.empty()){
-                    int frontnode=q.front();
-                    q.pop();
-                    
-                    //store front in ans
-                    ans.push_back(frontnode);
-                    
-                    //traverse all neighbours of front node
-                    for(auto i:adj[frontnode]){
-                        if(!visited[i]){
-                            q.push(i);
-                            visited[i]=1;
-                        }
-                    }
-                }
-            }
-    // Function to return Breadth First Traversal of given graph.
+  void solveBFS(vector<vector<int>>&adj,unordered_map<int,bool>&visited,vector<int>&ans,int node){
+      queue<int>q;
+      q.push(node);
+      visited[node]=1;
+      while(!q.empty()){
+          int frontnode=q.front();
+          q.pop();
+          ans.push_back(frontnode);
+          for(auto i:adj[frontnode]){
+              if(!visited[i]){
+                  q.push(i);
+                  visited[i]=1;
+              }
+          }
+      }
+  }
     vector<int> bfs(vector<vector<int>> &adj) {
-        // Code here
+        // code here
         int vertex=adj.size();
-        unordered_map<int,bool>visited;
         vector<int>ans;
+        unordered_map<int,bool>visited;
+        
         for(int i=0;i<vertex;i++){
             if(!visited[i]){
-                bfs(adj,visited,ans,i);
+                solveBFS(adj,visited,ans,i);
             }
         }
         return ans;
+        
     }
 };
