b9a14699aa47dbb526f0989df281282fee6edbdf
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-01-08 21:56:49 +0530

    Time: 0 ms (100%), Space: 7.7 MB (68.55%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int countNumbersWithUniqueDigits(int n) {
-        // Optimization: Pigeonhole Principle.
-        // We only have 10 unique digits (0-9). 
-        // Any number with length > 10 must have duplicates.
-        if (n > 10) {
-            return countNumbersWithUniqueDigits(10);
-        }
+        // Digits sirf 0–9 hain 11 digits ka number unique ho hi nahi sakta if
+        if(n > 10) { return countNumbersWithUniqueDigits(10); }
 
-        // Base Case: For n=0, the range is 0 <= x < 1. 
-        // The only number is 0.
+        // base case
         if (n == 0) {
             return 1;
         }
 
-        // Step 1: Calculate unique numbers of length EXACTLY n
-        // Start with 9 choices for the first digit (1-9)
+        // for ex man lo n=3
+        //  choice _ _ _
+        // pehli choice 0 nahi hoo skata= 1-9 = 9
+        // dusri choice 0-9 but pehli choice exclude = 9
+        // tisri choice  2 digit hoo chuke = 8
         int uniquenumbers_len_n = 9;
         int available_digits = 9;
 
@@ -26,7 +24,32 @@ public:
             available_digits--;
         }
 
-        // Step 2: Recursive call for n-1 and add current length count
+        // recursive call kyu?
+        // n = 3 ka answer =
+        // 1-digit unique numbers
+        // 2-digit unique numbers
+        // 3-digit unique numbers
         return countNumbersWithUniqueDigits(n - 1) + uniquenumbers_len_n;
     }
 };
+
+
+//recursive dry run
+// Dry Run: n = 2
+// Step 1:
+// unique length 2 = 9 × 9 = 81
+
+// Step 2:
+// countNumbersWithUniqueDigits(1) = 10
+
+// Final:
+// 10 + 81 = 91
+
+// Dry Run: n = 3
+// unique length 3 = 9 × 9 × 8 = 648
+// countNumbersWithUniqueDigits(2) = 91
+
+
+// Final:
+
+// 648 + 91 = 739
