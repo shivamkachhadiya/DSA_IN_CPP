3e01ac475a0674a12d459d4106ad2a24f86465f0
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-01-12 22:02:03 +0530

    Time: 44 ms (35.28%), Space: 49.5 MB (24.91%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     const int INF = 1e9;
 
-    int solve(vector<int>& coins, int target, int i) {
+    int solve(vector<int>& coins, int i, int target, vector<vector<int>>& dp) {
+
+        // base case
         if (target == 0) return 0;
 
         if (i == 0) {
@@ -12,18 +14,26 @@ public:
                 return INF;
         }
 
-        int notake = solve(coins, target, i - 1);
+        // memo check
+        if (dp[i][target] != -1)
+            return dp[i][target];
+
+        // not take current coin
+        int notake = solve(coins, i - 1, target, dp);
+
+        // take current coin (unbounded)
         int take = INF;
-
         if (coins[i] <= target)
-            take = 1 + solve(coins, target - coins[i], i);
+            take = 1 + solve(coins, i, target - coins[i], dp);
 
-        return min(take, notake);
+        return dp[i][target] = min(take, notake);
     }
 
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        int ans = solve(coins, amount, n - 1);
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+
+        int ans = solve(coins, n - 1, amount, dp);
         return (ans >= INF) ? -1 : ans;
     }
 };
