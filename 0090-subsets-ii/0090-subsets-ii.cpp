3155c4015af39b22b211741496296209dfd1f19c
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-01-13 18:36:32 +0530

    Time: 0 ms (100%), Space: 10.3 MB (84.29%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    void solve(vector<int>& arr, int i, int n, vector<int>& part,
+    void solve(vector<int>& arr, int i, int n,
+               vector<int>& part,
                vector<vector<int>>& ans) {
+
         if (i == n) {
             ans.push_back(part);
             return;
         }
-        // include
+
+        // include current element
         part.push_back(arr[i]);
         solve(arr, i + 1, n, part, ans);
         part.pop_back();
-                while (i + 1 < n && arr[i] == arr[i + 1]) {
+
+        // exclude current element
+        // skip all duplicates
+        while (i + 1 < n && arr[i] == arr[i + 1]) {
             i++;
         }
         solve(arr, i + 1, n, part, ans);
-        // backtrack
-        // return ans;
     }
+
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-
-        int n = nums.size();
         vector<vector<int>> ans;
         vector<int> part;
-        solve(nums, 0, n, part, ans);
+        solve(nums, 0, nums.size(), part, ans);
         return ans;
     }
 };
