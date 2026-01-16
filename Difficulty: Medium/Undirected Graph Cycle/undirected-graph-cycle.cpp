5808bce8c05ebc45c378c883c3c3c731bb220fe9
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-01-16 18:37:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,48 +1,36 @@
-#include<unordered_map>
-#include<list>
-#include<queue>
 class Solution {
   public:
-    bool isCyclicBFS(int src,unordered_map<int,bool>&visited,
-    unordered_map<int,list<int>>&adj){
-        unordered_map<int,int>parent;
-        parent[src]=-1;
-        visited[src]=-1;
-        queue<int>q;
-        q.push(src);
-        while(!q.empty()){
-            int front=q.front();
-            q.pop();
-            for(auto neighbour:adj[front]){
-                if(visited[neighbour]==true && neighbour!=parent[front]){
+    bool isCyclicDFS(int node,int parent,unordered_map<int,bool>&visited,
+    vector<vector<int>> &adj){
+        visited[node]=true;
+        for(auto nbs:adj[node]){
+            if(!visited[nbs]){
+                bool cycle=isCyclicDFS(nbs,node,visited,adj);
+                if(cycle){
                     return true;
-                }else if(!visited[neighbour]){
-                    q.push(neighbour);
-                    visited[neighbour]=1;
-                    parent[neighbour]=front;
                 }
+            }else if(nbs!=parent){
+                return true;
             }
         }
         return false;
     }
-
-
     bool isCycle(int V, vector<vector<int>>& edges) {
         // Code here
-        int n=edges.size();
-        int m=edges[0].size();
-        //create adjacency list
-        unordered_map<int,list<int>>adj;
-        for(int i=0;i<n;i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
+        vector<vector<int>> adj(V);
+
+        // Build adjacency list
+        for (auto &e : edges) {
+            int u = e[0];
+            int v = e[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
+
         unordered_map<int,bool>visited;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<V;i++){
             if(!visited[i]){
-                bool ans=isCyclicBFS(i,visited,adj);
+                bool ans=isCyclicDFS(i,-1,visited,adj);
                 if(ans==1){
                     return true;
                 }
