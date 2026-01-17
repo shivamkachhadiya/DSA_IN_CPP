eb7061cf408ecd7cbf78fac97cd19b80e9b8f721
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-01-17 15:49:56 +0530

    Time: 0 ms (100%), Space: 9 MB (38.29%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    bool isValid(string str) {
-        stack<char>stack;
-        for(int i=0;i<str.size();i++){
-            if(str[i]=='('||str[i]=='{'||str[i]=='['){
-                stack.push(str[i]);
-            }else{
-                if(stack.size()==0){
-                    return false;
+    bool isValid(string s) {
+        stack<char> st;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if (st.empty()) return false; 
+                if (!st.empty()) {
+                    char top = st.top();
+                    if ((ch == ')' && st.top() == '(') ||
+                        (ch == '}' && st.top() == '{') ||
+                        (ch == ']' && st.top() == '[')) {
+                        st.pop();
+                    } else {
+                        return false;
+                    }
                 }
-                if(stack.top()=='('&&str[i]==')' ||
-                   stack.top()=='{'&&str[i]=='}' ||
-                   stack.top()=='['&&str[i]==']'){
-                    stack.pop();
-                   }else{
-                    return false;
-                   }
             }
         }
-        return stack.size()==0;
+        return st.empty();
     }
 };
