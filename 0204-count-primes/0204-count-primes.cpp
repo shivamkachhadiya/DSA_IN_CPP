bee0378b7f1f5b271be40dd4bddb8168a5935b30
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-02-01 14:39:12 +0530

    Time: 210 ms (28.54%), Space: 29.8 MB (24.26%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n==0)return 0;
+        if(n==1)return 0;
+        int prime[n+1];
+        for(int i=2;i<n;i++){
+            prime[i]=1;
+        }
+        for(int i=2;i<n;i++){
+            if(prime[i]==1){
+                for(int j=2*i;j<=n;j+=i){
+                    prime[j]=0;
+                }
+            }
+        }
+        int count=0;
+        for(int i=2;i<n;i++){
+            if(prime[i]==1){
+                count++;
+            }
+        }
+        return count;
+    }
+};
