d87e65f1f67795c94539a4c8ab8793c7424d07f2
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-02-03 20:01:32 +0530

    Time: 0 ms (100%), Space: 16.9 MB (84.21%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class Solution {
+    map<pair<int, int>, vector<TreeNode*>> memo;
+
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        if (n == 0) return {};
+        return buildTrees(1, n);
+    }
+
+    vector<TreeNode*> buildTrees(int start, int end) {
+        if (start > end) return {nullptr};
+        if (memo.count({start, end})) return memo[{start, end}];
+
+        vector<TreeNode*> allTrees;
+        for (int i = start; i <= end; ++i) {
+            vector<TreeNode*> leftSubtrees = buildTrees(start, i - 1);
+            vector<TreeNode*> rightSubtrees = buildTrees(i + 1, end);
+
+            for (auto left : leftSubtrees) {
+                for (auto right : rightSubtrees) {
+                    TreeNode* root = new TreeNode(i, left, right);
+                    allTrees.push_back(root);
+                }
+            }
+        }
+        return memo[{start, end}] = allTrees;
+    }
+};
