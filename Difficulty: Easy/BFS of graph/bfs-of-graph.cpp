2b0b0cdd5b01025da25b28fa8b480025c89c6a60
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-02-15 10:10:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
@@ -1,33 +1,33 @@
 class Solution {
   public:
-  void solveBFS(vector<vector<int>>&adj,unordered_map<int,bool>&visited,vector<int>&ans,int node){
-      queue<int>q;
-      q.push(node);
-      visited[node]=1;
-      while(!q.empty()){
-          int frontnode=q.front();
-          q.pop();
-          ans.push_back(frontnode);
-          for(auto i:adj[frontnode]){
-              if(!visited[i]){
-                  q.push(i);
-                  visited[i]=1;
-              }
-          }
-      }
-  }
+  void bfs(vector<vector<int>> &adj,unordered_map<int,bool>&vis,
+            vector<int>&ans,int node){
+                queue<int>q;
+                q.push(node);
+                vis[node]=1;
+                while(!q.empty()){
+                    int front=q.front();
+                    q.pop();
+                    ans.push_back(front);
+                    for(auto i:adj[front]){
+                        if(!vis[i]){
+                            q.push(i);
+                            vis[i]=1;
+                        }
+                    }
+                }
+            }
     vector<int> bfs(vector<vector<int>> &adj) {
         // code here
-        int vertex=adj.size();
+        unordered_map<int,bool>vis;
+        queue<int>q;
         vector<int>ans;
-        unordered_map<int,bool>visited;
-        
+        int vertex=adj.size();
         for(int i=0;i<vertex;i++){
-            if(!visited[i]){
-                solveBFS(adj,visited,ans,i);
+            if(!vis[i]){
+                bfs(adj,vis,ans,i);
             }
         }
         return ans;
-        
     }
 };
