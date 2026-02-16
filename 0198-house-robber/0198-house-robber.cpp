f24f3cd2fde39d33158871dd12eadde39f7fb1e0
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-02-16 13:16:15 +0530

    Time: 0 ms (100%), Space: 10.6 MB (44.66%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,52 +1,22 @@
 class Solution {
 public:
-    int solve(int index, vector<int>& nums, vector<int>& dp) {
-
-        if (index == 0)
-            return nums[index];
-        if (index < 0)
+    int solve(vector<int>&arr,int n,int i,vector<int>&dp){
+        if(i<0){
             return 0;
-        if(dp[index]!=-1){
-            return dp[index];
+        }
+        if(i==0){
+            return arr[0];
         }
 
-        int pick = nums[index] + solve(index - 2, nums,dp);
+        if(dp[i]!=-1)return dp[i];
 
-        int not_pick = 0 + solve(index - 1, nums,dp);
-
-        dp[index]= max(pick, not_pick);
-
-        return dp[index];
+        int pick=arr[i]+solve(arr,n,i-2,dp);
+        int nonpick=0+solve(arr,n,i-1,dp);
+        return dp[i]=max(pick,nonpick);
     }
     int rob(vector<int>& nums) {
-
-        // recursion + memoization
-
-        // int n = nums.size();
-        // vector<int>dp(n,-1);
-        // return solve(n - 1, nums,dp);
-
-        //--------------------recursion + memoization + tabulation-----------------------
-
-        int n = nums.size();
-        if (n == 0)
-            return 0;
-        if (n == 1)
-            return nums[0];
-
-        vector<int> dp(n, 0);
-
-        // base cases
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-
-        // fill dp array iteratively
-        for (int i = 2; i < n; i++) {
-            int pick = nums[i] + dp[i - 2]; // if we rob current house
-            int not_pick = dp[i - 1];       // if we skip current house
-            dp[i] = max(pick, not_pick);
-        }
-
-        return dp[n - 1];
+         int n=nums.size();
+         vector<int>dp(n+1,-1);
+        return solve(nums,n,n-1,dp);
     }
 };
