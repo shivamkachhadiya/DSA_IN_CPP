5f26058a674e592c2ed66949b01c4c0b42e5408f
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-02-17 22:28:14 +0530

    Time: 0 ms (100%), Space: 21.8 MB (37.43%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int xor_all = 0;
-        int xor_arr = 0;
-        int n = nums.size();
-
-        // XOR all numbers from 0 to n
-        for (int i = 0; i <= n; i++) {
-            xor_all ^= i;
+        int n=nums.size();
+        int total=0;
+        for(int i=1;i<=n;i++){
+            total+=i;
         }
 
-        // XOR all elements in nums
-        for (int i = 0; i < n; i++) {
-            xor_arr ^= nums[i];
+        int actualsum=0;
+        for(auto i:nums){
+            actualsum+=i;
         }
 
-        // XOR of xor_all and xor_arr gives the missing number
-        return xor_all ^ xor_arr;
+
+        return total-actualsum;
     }
 };
