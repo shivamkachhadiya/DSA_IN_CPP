518bbba4fd9bda8a7d112864149ab6aa98c077c1
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-02-20 18:16:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,43 +1,41 @@
-#include <queue>
-using namespace std;
-
 class MyStack {
-    queue<int> q; // only one queue used to simulate stack
-
 public:
     MyStack() {
-        // Constructor – nothing to initialize since we use STL queue
+        
     }
-
-    // Push operation: O(n) time
+    
     void push(int x) {
-        int s = q.size(); // get current queue size
-        q.push(x);        // insert new element at back
+        int s = q.size(); 
+        q.push(x);        
 
-        // Rotate all old elements behind the new one
         for (int i = 0; i < s; i++) {
             q.push(q.front());
             q.pop();
         }
-
-        // Now x is at the front of the queue = top of stack
     }
-
-    // Pop operation: O(1) time
+    
     int pop() {
-        // Front of queue = top of stack
         int val = q.front();
         q.pop();
         return val;
     }
-
-    // Top operation: O(1) time
+    
     int top() {
-        return q.front(); // return the front of the queue
-    }
+                return q.front(); 
 
-    // Empty operation: O(1) time
+    }
+    
     bool empty() {
-        return q.empty(); // check if queue is empty
+                return q.empty(); 
+
     }
 };
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
