a2383f302534daee499405e217597ab6a92ccf33
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-02-21 11:33:08 +0530

    Time: 0 ms (100%), Space: 22.2 MB (9.02%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -6,30 +6,25 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int>& data) {
-        if (root == NULL) {
-            return;
-        }
+    void inorder(TreeNode* root,vector<int>&data){
+        if(root==NULL)return;
+        
         inorder(root->left,data);
         data.push_back(root->val);
         inorder(root->right,data);
+        
     }
     bool isValidBST(TreeNode* root) {
-        if (root == nullptr)
-            return true;
-
-        vector<int> data;
-        inorder(root, data);
-
-        // inorder traversal of BST must be strictly increasing
-        for (int i = 1; i < data.size(); i++) {
-            if (data[i] <= data[i - 1]) {
+        if(root==NULL)return true;
+        vector<int>data;
+        inorder(root,data);
+        for(int i=1;i<data.size();i++){
+            if(data[i]<=data[i-1]){
                 return false;
             }
         }
