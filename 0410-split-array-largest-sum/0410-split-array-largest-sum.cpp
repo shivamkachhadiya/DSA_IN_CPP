ad1244b09f9e050dc1543a596ce6359bd51ac466
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-03-23 22:56:40 +0530

    Time: 0 ms (100%), Space: 10.5 MB (62.39%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,41 +1,44 @@
 class Solution {
-public:
-    bool isvalid(vector<int>& arr,int n,int k,int mid){
-        int student=1,pages=0;
-        for(int i=0;i<n;i++){
-            if(arr[i]>mid){
-                return false;
-            }
-            else if(arr[i]+pages<=mid){
-                pages=pages+arr[i];
-            }else{
+private:
+    bool possible(vector<int>& arr, int n, int m, int mid) {
+        int student = 1;
+        int page = 0;
+        for (int i = 0; i < n; i++) {
+
+            if (page + arr[i] <= mid) {
+                page += arr[i];
+            } else {
+                
                 student++;
-                pages=arr[i];
+                if (student > m || arr[i] > mid) {
+                    return false;
+                }
+                page = 0;
+                page += arr[i];
             }
-        }
-        if(student>k){
-            return false;
-        }else{
-            return true;
         }
 
+        return true;
     }
+
+public:
     int splitArray(vector<int>& arr, int k) {
-        int n=arr.size();
-        int low=0;
-        int high=accumulate(arr.begin(), arr.end(), 0);
-        int ans=0;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(isvalid(arr,n,k,mid)){
-                ans=mid;
-                high=mid-1;
-            }else{
-                low=mid+1;
+        int n = arr.size();
+        int s = 0, sum = 0;
+        for (auto i : arr) {
+            sum += i;
+        }
+        int e = sum;
+        int ans = -1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (possible(arr, n, k, mid)) {
+                ans = mid;
+                e = mid - 1;
+            } else {
+                s = mid + 1;
             }
         }
         return ans;
-
-        
     }
 };
