f528ee7665108dadfc97485e0d126d0683d2123a
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-03-25 22:51:59 +0530

    Time: 203 ms (10.83%), Space: 139.8 MB (10.71%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
-    // We use a 2D vector for memoization: dp[index][current_sum]
-    // Using int instead of bool to store three states: -1 (unvisited), 0 (false), 1 (true)
-    bool solve(vector<int>& nums, int n, int i, int sum, int tar, vector<vector<int>>& dp) {
+    bool solve(vector<int>& nums, int n, int i, int sum, int tar,
+               vector<vector<int>>& dp) {
         if (sum == tar)
             return true;
         if (i >= n || sum > tar)
             return false;
-        
-        // Fix 1: Check the 2D DP state
         if (dp[i][sum] != -1) {
             return dp[i][sum];
         }
 
         bool take = solve(nums, n, i + 1, sum + nums[i], tar, dp);
+
         bool notake = solve(nums, n, i + 1, sum, tar, dp);
-
-        // Fix 2: Store the result in the 2D DP state
-        return dp[i][sum] = (take || notake);
+        return dp[i][sum] = take || notake;
     }
-
     bool canPartition(vector<int>& nums) {
-        int totalSum = 0;
+        int sum = 0;
         for (int i : nums) {
-            totalSum += i;
+            sum += i;
         }
-
-        if (totalSum % 2 != 0) return false;
-
         int n = nums.size();
-        int tar = totalSum / 2;
-
-        // Fix 3: Initialize a 2D DP table [index][sum]
-        // Size: (number of elements) x (possible sum target + 1)
-        vector<vector<int>> dp(n, vector<int>(tar + 1, -1));
-
-        return solve(nums, n, 0, 0, tar, dp);
+        vector<vector<int>> dp(n, vector<int>(sum/2 + 1, -1));
+        if (sum % 2 == 0) {
+            return solve(nums, nums.size(), 0, 0, sum / 2, dp);
+        } else {
+            return false;
+        }
     }
 };
