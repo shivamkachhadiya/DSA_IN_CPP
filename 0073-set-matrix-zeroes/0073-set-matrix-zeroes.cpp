338c40625566f80251cdf56205ed0a7d7ad26309
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-03-26 17:53:09 +0530

    Time: 0 ms (100%), Space: 20.9 MB (9.48%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-
-        vector<int> row(n, 0);
-        vector<int> col(m, 0);
-
-        // First pass: mark rows and columns to be zeroed
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == 0) {
-                    row[i] = 1;
-                    col[j] = 1;  // FIXED from col[i]
+    void setZeroes(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<int>row(n,0);
+        vector<int>col(m,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
                 }
             }
         }
-
-        // Second pass: set zeroes
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (row[i] == 1 || col[j] == 1) {  // FIXED condition
-                    matrix[i][j] = 0;             // FIXED assignment
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(row[i]||col[j]){
+                    mat[i][j]=0;
                 }
             }
         }
+        
     }
 };
