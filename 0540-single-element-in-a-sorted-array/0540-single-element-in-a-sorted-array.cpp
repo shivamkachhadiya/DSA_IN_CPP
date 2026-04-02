7af3046bb6347c0f8da97458137cf475a40c4996
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-04-02 18:45:46 +0530

    Time: 13 ms (3.57%), Space: 31.3 MB (6.41%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& arr) {
-        int n = arr.size();
-        if (n == 1) return arr[0];
-
-        for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                if (arr[i] != arr[i + 1])
-                    return arr[i];
-            } else if (i == n - 1) {
-                if (arr[i] != arr[i - 1])
-                    return arr[i];
-            } else {
-                if (arr[i] != arr[i - 1] && arr[i] != arr[i + 1])
-                    return arr[i];
+    int singleNonDuplicate(vector<int>& nums) {
+        unordered_map<int,int>freq;
+        for(int i=0;i<nums.size();i++){
+            freq[nums[i]]++;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(freq[nums[i]]==1){
+                return nums[i];
             }
         }
-
-        return -1; // Should never reach here if input is valid
+        return -1;
     }
 };
