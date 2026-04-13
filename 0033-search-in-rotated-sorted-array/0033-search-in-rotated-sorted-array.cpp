e1ba316a400e61dd461bb7864ff882b6e6d3d61c
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-04-13 21:28:33 +0530

    Time: 0 ms (100%), Space: 15.3 MB (34.49%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int target) {
-        int start = 0, end = arr.size() - 1;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (arr[mid] == target) return mid;
-
-            // Left half is sorted
-            if (arr[start] <= arr[mid]) {
-                if (arr[start] <= target && target < arr[mid])
-                    end = mid - 1;
-                else
-                    start = mid + 1;
-            } 
-            // Right half is sorted
-            else {
-                if (arr[mid] < target && target <= arr[end])
-                    start = mid + 1;
-                else
-                    end = mid - 1;
+    int search(vector<int>& arr, int key) {
+         int n=arr.size();
+        int start=0;
+        int end=n-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(arr[mid]==key)return mid;
+            if(arr[start]<=arr[mid]){
+                //left half sorted
+                if(arr[start]<=key && key<=arr[mid]){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
+            }else{
+                //right half sorted
+                if (arr[mid] <= key && key <= arr[end]) {
+                    start=mid+1;
+                }else{
+                    end=mid-1;
+                }
             }
         }
-
         return -1;
     }
 };
