aabe715035863355051d686b1083c41459d8a782
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-04-13 22:16:10 +0530

    Time: 1 ms (45.27%), Space: 77.2 MB (12.07%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -3,33 +3,40 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode() : val(0), next(NULLptr) {}
- *     ListNode(int x) : val(x), next(NULLptr) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(0);
+        ListNode* temp1 = l1;
+        ListNode* temp2 = l2;
+        ListNode* newHead = new ListNode(0);
+        ListNode* temp3 = newHead;
         int carry = 0;
-        ListNode* temp = dummy;
-        while (l1 != NULL || l2 != NULL || carry != 0) {
-                    int x = 0, y = 0;
 
-            if (l1 != NULL)
-                x = l1->val;
-            if (l2 != NULL)
-                y = l2->val;
-            int sum = x + y + carry;
-            carry = sum / 10;
-            temp->next = new ListNode(sum % 10);
-            temp=temp->next;
-            if (l1 != NULL)
-                l1 = l1->next;
-            if (l2 != NULL)
-                l2 = l2->next;
+        while (temp1 != NULL || temp2 != NULL || carry != 0) {
+            int a = 0;
+            int b = 0;
+            if (temp1 != NULL) {
+                a = temp1->val;
+                temp1 = temp1->next;
+            }
+
+            if (temp2 != NULL) {
+                b = temp2->val;
+                temp2 = temp2->next;
+            }
+            int c = a + b + carry;
+            int ans = c % 10;
+            carry = c / 10;
+
+            temp3->next = new ListNode(ans);
+            temp3 = temp3->next;
+           
         }
-        return dummy->next;
+        return newHead->next;
     }
 };
