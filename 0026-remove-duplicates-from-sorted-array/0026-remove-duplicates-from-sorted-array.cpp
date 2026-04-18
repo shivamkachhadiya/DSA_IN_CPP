2b06bc023f6138ae7c68560cc2035e6177be0dd8
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-04-18 13:20:18 +0530

    Time: 0 ms (100%), Space: 22.6 MB (51.71%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    int i = 0;
     int removeDuplicates(vector<int>& nums) {
-       
-        for (int j=1; j < nums.size(); j++) {
-            if (nums[i] != nums[j]) {
-                i++;
-                nums[i] = nums[j];
+        int uniqueElements=1;
+        //int cm=1;
+        int officer=0;
+        int n=nums.size();
+       for(int cm=1;cm<n;cm++){
+            if(nums[cm]!=nums[cm-1]){
+                //pichhe vale ke barabar nahi hei
+                nums[officer+1]=nums[cm];
+                officer++;
+                uniqueElements++;
             }
         }
-
-        return i + 1;
+        return uniqueElements;
     }
 };
