6a83448a6f4d9b8ea101ca615118b199a40b4913
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-04-22 15:15:57 +0530

    Time: 3 ms (74.87%), Space: 12.6 MB (70.59%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -3,18 +3,18 @@ public:
     vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
         vector<string> result;
       
-        for (auto& queryWord : queries) {
-            for (auto& dictWord : dictionary) {
-                int differenceCount = 0;
+        for (auto& q_w : queries) {
+            for (auto& dict_w : dictionary) {
+                int d_count = 0;
               
-                for (int i = 0; i < queryWord.size(); ++i) {
-                    if (queryWord[i] != dictWord[i]) {
-                        differenceCount++;
+                for (int i = 0; i < q_w.size(); ++i) {
+                    if (q_w[i] != dict_w[i]) {
+                        d_count++;
                     }
                 }
               
-                if (differenceCount <= 2) {
-                    result.emplace_back(queryWord);
+                if (d_count <= 2) {
+                    result.emplace_back(q_w);
                     break;  
                 }
             }
