f6deeec38e1c8e89d16a97ea8d46490f43527d52
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-04-25 20:19:52 +0530

    Time: 10 ms (94.86%), Space: 17.6 MB (21.56%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end()); // Pehle array ko sort karte hain
+        
+        int n = nums.size();
+        for (int i = 0; i < n - 3; i++) {
+            //  Duplicate avoid karne ke liye
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            for (int j = i + 1; j < n - 2; j++) {
+                // Duplicate avoid karne ke liye
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                
+                long long newTarget = (long long)target - nums[i] - nums[j];
+                int left = j + 1, right = n - 1;
+                
+                while (left < right) {
+                    long long sum = nums[left] + nums[right];
+                    
+                    if (sum == newTarget) {
+                        res.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        
+                        // Left aur Right ko move karte hain aur duplicates skip karte hain
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        
+                        left++;
+                        right--;
+                    } 
+                    else if (sum < newTarget) {
+                        left++; // Sum chhota hai to left badhao
+                    } 
+                    else {
+                        right--; // Sum bada hai to right kam karo
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
