2efd801d8b2f393937c7cfab9c3cb7f80c1adcfd
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-05-16 13:52:02 +0530

    Time: 447 ms (5.02%), Space: 25.9 MB (7.73%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -6,24 +6,21 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int height(TreeNode* root) {
-        if (root == NULL)
-            return 0;
-        return 1 + max(height(root->left), height(root->right));
+    int height(TreeNode* root){
+        if(root==NULL)return 0;
+        return 1+max(height(root->right),height(root->left));
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        if (root == NULL)
-            return 0;
-        int leftH=height(root->left);
-        int rightH=height(root->right);
-        int Ldiameter=diameterOfBinaryTree(root->left);
-        int Rdiameter=diameterOfBinaryTree(root->right);
-        return max({leftH+rightH,Ldiameter,Rdiameter});
+        if(root==NULL)return 0;
+        int left_height=height(root->left);
+        int right_height=height(root->right);
+        int leftRecursive=diameterOfBinaryTree(root->left);
+        int rightRecursive=diameterOfBinaryTree(root->right);
+        return max({left_height+right_height,leftRecursive,rightRecursive});
     }
 };
