4a71eda2013982177db64849c0d0f697d1397e7f
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-05-16 16:31:08 +0530

    Time: 0 ms (100%), Space: 21.4 MB (65.83%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -12,12 +12,9 @@
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
-        function<bool(TreeNode*, int)> dfs = [&](TreeNode* root, int s) -> int {
-            if (!root) return false;
-            s += root->val;
-            if (!root->left && !root->right && s == targetSum) return true;
-            return dfs(root->left, s) || dfs(root->right, s);
-        };
-        return dfs(root, 0);
+         if (!root) return false;
+        targetSum -= root->val;
+        if(root->left==NULL&&root->right==NULL)return targetSum==0;
+        return hasPathSum(root->left,targetSum)||hasPathSum(root->right,targetSum);
     }
 };
