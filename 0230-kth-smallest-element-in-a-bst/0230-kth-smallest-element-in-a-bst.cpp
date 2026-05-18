1f2c9c6aad1bb516bc8fc60c1b0731962223f5dc
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-05-18 20:27:47 +0530

    Time: 0 ms (100%), Space: 24.4 MB (38.33%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -6,24 +6,27 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    vector<int> temp;
-    void solveVec(TreeNode* root, int k) {
-        if (root == NULL)
+    int count=0;
+    int ans=0;
+    void solve(TreeNode* root,int k){
+        if(root==NULL){
             return;
-        solveVec(root->left, k);
-        temp.push_back(root->val);
-        solveVec(root->right, k);
-        int size = temp.size();
+        }
+        solve(root->left,k);
+        count++;
+        if(count==k){
+             ans=root->val;
+             return;
+        }
+        solve(root->right,k);
     }
     int kthSmallest(TreeNode* root, int k) {
-         temp.clear();    
-        solveVec(root, k);
-        return temp[k - 1];        // kth smallest (1‑indexed → k‑1)
+        solve(root,k);
+        return ans;
     }
 };
