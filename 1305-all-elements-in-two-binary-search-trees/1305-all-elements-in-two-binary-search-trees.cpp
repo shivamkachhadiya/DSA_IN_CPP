0c8230483e520c870c9a620f9b99039db432cac7
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-05-18 23:30:17 +0530

    Time: 4 ms (73.68%), Space: 87.1 MB (56.32%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -12,17 +12,17 @@
 class Solution {
 public:
     vector<int>ans1;
-    vector<int>ans2;
+    //vector<int>ans2;
     void solve(TreeNode* root){
         if(root==NULL)return;
-        ans1.push_back(root->val);
         solve(root->left);
+        ans1.push_back(root->val);
         solve(root->right);
     }
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
         solve(root1);
         solve(root2);
-        sort(ans1.begin(),ans1.end());
+       sort(ans1.begin(),ans1.end());
         return ans1;
     }
 };
