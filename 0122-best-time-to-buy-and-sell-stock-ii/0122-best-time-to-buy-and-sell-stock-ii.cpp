eb1d2fa21675693f6a59361f51f5f7b0472a29ba
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-05-24 15:43:37 +0530

    Time: 16 ms (6.43%), Space: 27.4 MB (15.49%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-    int solve(vector<int>&arr,int i,int n,int buy,vector<vector<int>>&dp){
-        if(i==n)return 0;
+    int solve(int i, int buy, vector<int>& prices,vector<vector<int>>&dp) {
+        if (i == prices.size())
+            return 0;
         if(dp[i][buy]!=-1)return dp[i][buy];
-        int profit=0;
-        if(buy){
-            profit=max(-arr[i]+solve(arr,i+1,n,0,dp),0+solve(arr,i+1,n,1,dp));
-        }else{
-            profit=max(arr[i]+solve(arr,i+1,n,1,dp),0+solve(arr,i+1,n,0,dp));
+        int profit = 0;
+
+        if (buy) {
+            int buykaro = -prices[i] + solve(i + 1, 0, prices,dp);
+            int skipkaro = 0 + solve(i + 1, 1, prices,dp);
+            profit = max(buykaro, skipkaro);
+        } else {
+            int sellkaro=+prices[i] + solve(i + 1, 1, prices,dp);
+            int skipkaro=0 + solve(i + 1, 0, prices,dp);
+            profit=max(sellkaro,skipkaro);
         }
         return dp[i][buy]=profit;
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(2,-1));
-        return solve(prices,0,prices.size(),1,dp);
-    }
+        vector<vector<int>>dp(n,vector<int>(2,-1)); 
+        return solve(0, 1, prices,dp);
+        }
 };
