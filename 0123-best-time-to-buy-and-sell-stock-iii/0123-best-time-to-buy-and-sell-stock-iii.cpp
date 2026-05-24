95180866c74b6a4cf0bba3680d0a3dac7fc39bef
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-05-24 16:50:51 +0530

    Time: 461 ms (16.33%), Space: 225.4 MB (13.66%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-    int solveDP(int index, int buy, vector<int>& prices,
-                vector<vector<vector<int>>>& dp, int limits) {
-        if (index == prices.size() || limits == 0) {
-            return 0;
+    int solve(int i,int buy,vector<int>&prices,int limit,vector<vector<vector<int>>>&dp){
+        if(i==prices.size())return 0;
+        if(limit==0)return 0;
+        if(dp[i][buy][limit]!=-1)return dp[i][buy][limit];
+        int profit=0;
+        if(buy){
+            int buykaro=-prices[i]+solve(i+1,0,prices,limit,dp);
+            int skipkro=0+solve(i+1,1,prices,limit,dp);
+            profit=max(buykaro,skipkro);
+        }else{
+            int sellkaro=+prices[i]+solve(i+1,1,prices,limit-1,dp);
+            int skipkaro=0+solve(i+1,0,prices,limit,dp);
+            profit=max(sellkaro,skipkaro);
         }
-
-        if (dp[index][buy][limits] != -1)
-            return dp[index][buy][limits];
-
-        int profit = 0;
-        if (buy) {
-            // Buy or Skip
-            profit = max(-prices[index] + solveDP(index + 1, 0, prices, dp, limits),
-                         0 + solveDP(index + 1, 1, prices, dp, limits));
-        } else {
-            // Sell or Skip
-            profit = max(prices[index] + solveDP(index + 1, 1, prices, dp, limits - 1),
-                         0 + solveDP(index + 1, 0, prices, dp, limits));
-        }
-
-        return dp[index][buy][limits] = profit;
+        return dp[i][buy][limit]=profit;
     }
-
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        // dp[n][2][3] initialized with -1
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
-        return solveDP(0, 1, prices, dp, 2);
+        int n=prices.size();
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
+        return solve(0,1,prices,2,dp);
     }
 };
