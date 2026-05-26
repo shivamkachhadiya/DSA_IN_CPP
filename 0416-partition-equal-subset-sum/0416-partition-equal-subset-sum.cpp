f2fdd1962240f98106a58f9d67d3831b0f4845a2
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-05-27 00:03:31 +0530

    Time: 249 ms (6.55%), Space: 128.4 MB (12.32%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
-    bool solve(vector<int>&nums,int n,int i,int target,int sum,vector<vector<int>>&dp){
-        if(i>=n){
-            if(sum==target)return true;
-            else return false;
+    bool solve(vector<int>& arr, int target, int sum, int n, int i,
+               vector<vector<int>>& dp) {
+        if (i >= n) {
+            if (sum == target) {
+                return true;
+            } else {
+                return false;
+            }
         }
-        if(sum > target){
+        if (sum > target)
             return false;
+        if (sum == target)
+            return true;
+
+        if (dp[sum][i] != -1) {
+            return dp[sum][i];
         }
-        if(sum==target)return true;
 
-        if(dp[i][sum]!=-1)return dp[i][sum];
+        bool pick = solve(arr, target, sum + arr[i], n, i + 1, dp);
+        bool nonpick = solve(arr, target, sum, n, i + 1, dp);
 
-        bool pick=solve(nums,n,i+1,target,sum+nums[i],dp);
-        bool nonpick=solve(nums,n,i+1,target,sum,dp);
-        return dp[i][sum]=pick||nonpick;
-       
-
+        return dp[sum][i] = pick || nonpick;
     }
     bool canPartition(vector<int>& nums) {
-        int total=0;
-        for(auto i:nums){
-            total+=i;
+        int total = 0;
+        for (auto i : nums) {
+            total += i;
         }
-        if(total%2==0){
-            int target=total/2;
-            int n=nums.size();
-            vector<vector<int>>dp(n,vector<int>(target+1,-1));
-            return solve(nums,nums.size(),0,target,0,dp);
-        }else{
+        int n = nums.size();
+        if (total % 2 == 0) {
+            int target = total / 2;
+            vector<vector<int>> dp(target + 1, vector<int>(n, -1));
+            return solve(nums, target, 0, n, 0, dp);
+        } else {
             return false;
         }
     }
