ba7355b5e9b59cfee44b70dfee5d458f5c4679e2
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-05-27 17:58:05 +0530

    Time: 2 ms (37.04%), Space: 9.8 MB (80.49%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    void solve(vector<int>& nums,vector<vector<int>>&ans,vector<int>&part,int i,int n){
-        if(i==n){
+    void solve(vector<int>&nums,vector<vector<int>>&ans,vector<int>&part,int i){
+        if(i==nums.size()){
             ans.push_back(part);
-            return;            
+            return;
         }
-        //include
         part.push_back(nums[i]);
-        solve(nums,ans,part,i+1,n);
-
-        //exclude
+        solve(nums,ans,part,i+1);
         part.pop_back();
-        solve(nums,ans,part,i+1,n);
+        solve(nums,ans,part,i+1);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>>ans;
         vector<int>part;
-        int i=0;
-        int n=nums.size();
-        solve(nums,ans,part,i,n);
+        solve(nums,ans,part,0);
         return ans;
     }
 };
