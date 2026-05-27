5a887bad7895619ebe19ca6bc7f7a7a68bb79140
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-05-27 18:32:30 +0530

    Time: 3 ms (51.24%), Space: 14 MB (92.02%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,56 +1,30 @@
 class Solution {
 public:
-    // void findCombination(int ind, int target, vector<int>& arr,
-    //                      vector<vector<int>>& ans, vector<int>& ds) {
-    //     // if (ind == arr.size()) {
-    //     //     if (target == 0) {
-    //     //         ans.push_back(ds);
-    //     //     }
-    //     //     return;
-    //     // }
-
-    //     // // pick up element
-    //     // if (arr[ind] <= target) {
-    //     //     ds.push_back(arr[ind]);
-    //     //     findCombination(ind, target - arr[ind], arr, ans, ds);
-    //     //     ds.pop_back();
-    //     // }
-
-    //     // findCombination(ind + 1, target, arr, ans, ds);
-
-    // }
-  set<vector<int>> s;
-
-void getAllCombinations(vector<int>& arr, int idx, int tar, vector<vector<int>>& ans, vector<int>& combin) {
-    if(idx == arr.size() || tar < 0) {
-        return;
-    }
-
-    if(tar == 0) {
-        if(s.find(combin) == s.end()) {
-            ans.push_back(combin);
-            s.insert(combin);
+    void solve(vector<int>&arr,int target,vector<int>&part,vector<vector<int>>&ans,int i,int sum){
+        if(i>=arr.size()){
+            if(sum==target){
+                ans.push_back(part);
+            }
+                return;
+            
         }
-        return;
+
+        if(sum==target){
+            ans.push_back(part);
+            return;
+        }
+
+        if(sum>target)return;
+
+        part.push_back(arr[i]);
+        solve(arr,target,part,ans,i,sum+arr[i]);
+        part.pop_back();
+        solve(arr,target,part,ans,i+1,sum);
     }
-
-    combin.push_back(arr[idx]);
-    //single
-    getAllCombinations(arr, idx+1, tar - arr[idx], ans, combin);
-    //multiple
-    getAllCombinations(arr, idx, tar - arr[idx], ans, combin);
-    combin.pop_back();
-
-    //exclusion
-    getAllCombinations(arr, idx+1, tar, ans, combin);
-}
-
-    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
-        vector<vector<int>> ans;
-        vector<int>combin;
-        getAllCombinations(arr,0,target,ans,combin);
-        //vector<int> ds;
-        // findCombination(0, target, candidates, ans, ds);
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int>part;
+        vector<vector<int>>ans;
+        solve(candidates,target,part,ans,0,0);
         return ans;
     }
 };
