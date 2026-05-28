0a81f621d72f28261a4b47687802093b40649163
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-05-28 22:28:46 +0530

    Time: 7 ms (9.22%), Space: 96.5 MB (8.8%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-
-        int i=0;
-        int j=0;
-        int m=nums1.size();
-        int n=nums2.size();
-        vector<int>temp;
-        while(i<m&&j<n){
+        double i=0;
+        double j=0;
+        vector<double>ans;
+        double n1=nums1.size();
+        double n2=nums2.size();
+        while(i<n1 && j<n2){
             if(nums1[i]<=nums2[j]){
-                temp.push_back(nums1[i]);
+                ans.push_back(nums1[i]);
                 i++;
             }else{
-                temp.push_back(nums2[j]);
+                ans.push_back(nums2[j]);
                 j++;
             }
         }
-        while(i<m){
-            temp.push_back(nums1[i]);
+        while(i<n1){
+            ans.push_back(nums1[i]);
             i++;
         }
-        while(j<n){
-            temp.push_back(nums2[j]);
+        
+        while(j<n2){
+            ans.push_back(nums2[j]);
             j++;
         }
-
-        nums1 = temp;
-        int size = nums1.size();
-        int mid = size / 2;
-
-        if (size % 2 == 0) {
-            return (nums1[mid - 1] + nums1[mid]) / 2.0;  // ✅ fix for even
-        } else {
-            return nums1[mid];  // ✅ fix for odd
+        int length=ans.size();
+        if(length%2==0){
+            double start=0;
+            double end=length-1;
+            double mid=(start+end)/2;
+            double new_mid=mid+1;
+            double sum=ans[mid]+ans[new_mid];
+            return sum/2;
+        }else{
+             double start=0;
+            double end=length-1;
+            double mid=(start+end)/2;
+            return ans[mid]; 
         }
-
-        return -1;
-        
     }
 };
-
-
-
