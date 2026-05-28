0a91daa54fb899701cf57192047d05156b02bb90
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-05-28 22:12:10 +0530

    Time: 0 ms (100%), Space: 10.6 MB (42.36%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,42 +1,37 @@
 class Solution {
-private:
-    bool possible(vector<int>& arr, int n, int m, int mid) {
-        int student = 1;
-        int page = 0;
-        for (int i = 0; i < n; i++) {
-
-            if (page + arr[i] <= mid) {
-                page += arr[i];
-            } else {
-                
-                student++;
-                if (student > m || arr[i] > mid) {
-                    return false;
-                }
-                page = 0;
-                page += arr[i];
-            }
-        }
-
-        return true;
-    }
-
 public:
-    int splitArray(vector<int>& arr, int k) {
-        int n = arr.size();
-        int s = 0, sum = 0;
-        for (auto i : arr) {
-            sum += i;
+    bool solve(vector<int>&arr,int k,int mid){
+        int count=1;
+        int curr=0;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[i]>mid)return false;
+            else if(arr[i]+curr<=mid){
+                curr=curr+arr[i];
+            }else{
+                count++;
+                //curr=arr[mid]; 
+                curr=arr[i];
+            }
+            if(count>k)return false;
         }
-        int e = sum;
-        int ans = -1;
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-            if (possible(arr, n, k, mid)) {
-                ans = mid;
-                e = mid - 1;
-            } else {
-                s = mid + 1;
+     return true;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int start=0;
+        int ans=0;
+        int end=0;
+        int n=nums.size();
+         for(int i=0;i<n;i++){
+            end=end+nums[i];
+        }
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(solve(nums,k,mid)){
+                ans=mid;
+                end=mid-1;
+            }else{
+                start=mid+1;
             }
         }
         return ans;
