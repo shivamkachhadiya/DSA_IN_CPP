638f40a1ce525f80e5d47b942d5012824b958a29
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-05-29 16:30:30 +0530

    Time: 3 ms (3.55%), Space: 19.7 MB (8.15%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -8,36 +8,35 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* temp1 = list1;
-        ListNode* temp2 = list2;
-        ListNode* newNode = new ListNode(0);
-        ListNode* newPtr = newNode;
-        while (temp1 != nullptr && temp2 != nullptr) {
-            if (temp1->val <= temp2->val) {
-                newPtr->next = new ListNode(temp1->val);
-                newPtr = newPtr->next;
-                temp1 = temp1->next;
-            } else {
-                newPtr->next = new ListNode(temp2->val);
-                newPtr = newPtr->next;
-
-                temp2 = temp2->next;
+        ListNode* temp1=list1;
+        ListNode* temp2=list2;
+        ListNode* newDummyLinkList=new ListNode(0);
+        ListNode* tempPTR=newDummyLinkList;
+        while(temp1!=NULL && temp2!=NULL){
+            if(temp1->val <= temp2->val){
+                tempPTR->next=new ListNode(temp1->val);
+                tempPTR=tempPTR->next;
+                temp1=temp1->next;
+            }else{
+                tempPTR->next=new ListNode(temp2->val);
+                tempPTR=tempPTR->next;
+                temp2=temp2->next;
             }
         }
-        while(temp1!=nullptr){
-            newPtr->next=new ListNode(temp1->val);
-            newPtr=newPtr->next;
+        while(temp1!=NULL){
+            tempPTR->next=new ListNode(temp1->val);
+            tempPTR=tempPTR->next;
             temp1=temp1->next;
         }
-
-        while(temp2!=nullptr){
-            newPtr->next=new ListNode(temp2->val);
-                        newPtr=newPtr->next;
+        while(temp2!=NULL){
+            tempPTR->next=new ListNode(temp2->val);
+            tempPTR=tempPTR->next;
             temp2=temp2->next;
         }
-        return newNode->next;
-  }
+        return newDummyLinkList->next;
+    }
 };
