518f4a2f131b61f3629f8647cc498834d890c46e
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-06-04 14:45:56 +0530

    Time: 1276 ms (5.04%), Space: 712.3 MB (5.01%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int solve(string &text1,string &text2,int n1,int n2,int i,int j,vector<vector<int>>&dp){
-        if (i >= n1 || j >= n2) return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(text1[i]==text2[j]){
-            return 1+solve(text1,text2,n1,n2,i+1,j+1,dp);
+    int solve(string word1, string word2, int n1, int n2, int i, int j,vector<vector<int>>&dp) {
+        if (i >= n1 || j >= n2)
+            return 0;
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
-        else{
-            dp[i][j]= max(solve(text1,text2,n1,n2,i+1,j,dp),solve(text1,text2,n1,n2,i,j+1,dp));
+
+        int pick = 0;
+        if (word1[i] == word2[j]) {
+            pick = 1 + solve(word1, word2, n1, n2, i + 1, j + 1,dp);
         }
-        return dp[i][j];
-    }
-    int longestCommonSubsequence(string text1, string text2) {
-        int n1=text1.size();
-        int n2=text2.size();
-        int i=0;
-        int j=0;
-        vector<vector<int>>dp(n1,vector<int>(n2,-1));
-        return solve(text1,text2,n1,n2,i,j,dp);
+        int nonpick = max(solve(word1, word2, n1, n2, i + 1, j,dp),
+                          solve(word1, word2, n1, n2, i, j + 1,dp));
+        return dp[i][j]=max(pick, nonpick);
     }
     int minDistance(string word1, string word2) {
-        return word1.size()+word2.size()-2*longestCommonSubsequence(word1,word2);
+        int n1 = word1.size();
+        int n2 = word2.size();
+        vector<vector<int>>dp(n1,vector<int>(n2,-1));
+        return word1.size()+word2.size()-2*solve(word1, word2, n1, n2, 0, 0,dp);
+
     }
 };
