ddddade722c06d205dcab2f3a70deeaa119de791
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-06-07 16:44:56 +0530

    Time: 0 ms (100%), Space: 14.7 MB (96.01%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -6,26 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    vector<int>solve(TreeNode*root,vector<int>&ans,int level){
-        if(root==NULL)return ans;
-        if(level==ans.size()){
+    void solve(TreeNode* root, vector<int>&ans, int level) {
+        if (root == NULL)
+            return;
+        if (ans.size() == level) {
             ans.push_back(root->val);
         }
-        if(root->right){
-            solve(root->right,ans,level+1);
+        if (root->right) {
+            solve(root->right, ans, level + 1);
         }
+        if(root->left){
             solve(root->left,ans,level+1);
-        
-        return ans;
+        }
     }
     vector<int> rightSideView(TreeNode* root) {
-        vector<int>ans;
-        if(root==NULL)return ans;
-        return solve(root,ans,0);
+        vector<int> ans;
+        if (root == NULL)
+            return ans;
+        solve(root, ans, 0);
+        return ans;
     }
 };
