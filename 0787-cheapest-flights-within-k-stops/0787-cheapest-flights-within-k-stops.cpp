21e2d104a389a8d6f06c85c5caefe4100a8d5643
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-06-29 02:21:36 +0530

    Time: 0 ms (100%), Space: 17.5 MB (84.89%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -11,18 +11,18 @@ public:
             adj[u].push_back({v, wt});
         }
 
-        queue<pair<int, pair<int, int>>> q; //{node,{dist,stops}};
+        queue<pair<int, pair<int, int>>> q; //{top_node,{dist,stops}};
         vector<int> dist_v(n, INT_MAX);
         dist_v[src] = 0;
         q.push({src, {0, -1}});
 
         while (q.size() > 0) {
-            auto [node, info] = q.front();
+            auto [top_node, info] = q.front();
             q.pop();
 
             auto [dist, stops] = info;
 
-            for (auto& [neigh_node, neigh_dist] : adj[node]) {
+            for (auto& [neigh_node, neigh_dist] : adj[top_node]) {
                 if (dist + neigh_dist < dist_v[neigh_node] && stops + 1 <= k) {
                     dist_v[neigh_node] = dist + neigh_dist;
                     q.push({neigh_node, {dist + neigh_dist, stops + 1}});
