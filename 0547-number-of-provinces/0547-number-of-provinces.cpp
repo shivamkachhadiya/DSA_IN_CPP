9ec6a549b33feb3b578923db475ed76185e4dfbc
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-06-28 18:28:06 +0530

    Time: 0 ms (100%), Space: 19.4 MB (83.56%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>>& isConnected, vector<int>& visited) {
-        //jese hi new node visit kro mark in vector
-
-        visited[node] = 1;
-        
-        //ye for loop each row ke liye chalegi
-        
-        for (int j = 0; j < isConnected.size(); j++) {
-            //iska matalab node connected hona chahiye
-            // but usko vector mei visit mark na kiya ho matalb vo visted nahi hei
-            //visit krne ke liye dfs call lagaoo
-            if (isConnected[node][j] == 1 && !visited[j]) {
-                dfs(j, isConnected, visited);
+    int ans=0;
+    void dfs(int i,vector<vector<int>>& isConnected,vector<bool>&vis,int n){
+        vis[i]=true;
+        for(int j=0;j<n;j++){
+            if(isConnected[i][j]==1&&vis[j]==false){
+                vis[j]=true;
+                dfs(j,isConnected,vis,n);
             }
         }
     }
-
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<int> visited(n + 1, 0);
-        int count = 0;
-
-        //ye main outer loop hei.. jo 0 se mat.size tak jayegi
-        //jo vector mei mark rakhegi and count karegi
-        for (int i = 0; i < n; i++) {
-            if (visited[i] == 0) {
-                count++;
-                dfs(i, isConnected, visited);
+        int n=isConnected.size();
+        vector<bool>vis(n,false);
+        for(int i=0;i<n;i++){
+            if(vis[i]==false){
+                vis[i]=true;
+                dfs(i,isConnected,vis,n);
+                ans++;
             }
         }
-        return count;
+        return ans;
     }
 };
