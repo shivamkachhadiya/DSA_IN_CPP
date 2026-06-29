9e554bdcc87ea15cbba3c210722e78a03d51cb99
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-06-29 18:53:45 +0530

    Time: 7 ms (25.63%), Space: 31.9 MB (67.14%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
@@ -6,33 +6,31 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class BSTIterator {
 public:
-    vector<int> vals;
-    int cur;
-    BSTIterator(TreeNode* root) {
-        cur = 0;
-        inorder(root);
-    }
-
-    int next() {
-        return vals[cur++];
-    }
-
-    bool hasNext() {
-        return cur < vals.size();
-    }
-
-    void inorder(TreeNode* root) {
-        if (root) {
-            inorder(root->left);
-            vals.push_back(root->val);
-            inorder(root->right);
+    stack<TreeNode*> st;
+    void storeLeftNodes(TreeNode* root) {
+        while (root != NULL) {
+            st.push(root);
+            root = root->left;
         }
     }
+    BSTIterator(TreeNode* root) { storeLeftNodes(root); }
+
+    int next() {
+        TreeNode* ans = st.top();
+        st.pop();
+        if (ans->right != NULL) {
+            storeLeftNodes(ans->right);
+        }
+        return ans->val;
+    }
+
+    bool hasNext() { return !st.empty(); }
 };
 
 /**
