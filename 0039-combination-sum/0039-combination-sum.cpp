664932206514cc4260c25984b0e39d71f7f78ea9
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-07-01 05:19:18 +0530

    Time: 3 ms (52.66%), Space: 14.1 MB (75.62%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-    void solve(vector<int>&arr,int target,int n,vector<int>&part,vector<vector<int>>&ans,
-                int i,int sum){
-                
-                if(i==n){
-                    if(sum==target){
-                        ans.push_back(part);
-                        return;
-                    }
-                    return;
-                }
+    void solve(vector<int>& arr, int target, int n, vector<vector<int>>& ans,
+               vector<int>& part, int i, int sum) {
+        if (i == arr.size()) {
+            if (sum == target) {
+                ans.push_back(part);
+            }
+            return;
+        }
 
-                if(sum==target){
-                    ans.push_back(part);
-                    return;
-                }
+        if (sum > target) {
+            return;
+        }
+        if (sum == target) {
+            ans.push_back(part);
+            return;
+        }
 
-                if(sum>target){
-                    return;
-                }
-
-            //pick
-            part.push_back(arr[i]);
-            solve(arr,target,n,part,ans,i,sum+arr[i]);
-
-            part.pop_back();
-
-            solve(arr,target,n,part,ans,i+1,sum);
-
+        part.push_back(arr[i]);
+        solve(arr, target, n, ans, part, i, sum + arr[i]);
+        part.pop_back();
+        solve(arr, target, n, ans, part, i + 1, sum);
     }
     vector<vector<int>> combinationSum(vector<int>& arr, int target) {
-        int n=arr.size();
-        vector<int>part;
-        vector<vector<int>>ans;
-        solve(arr,target,n,part,ans,0,0);
+        int n = arr.size();
+        vector<vector<int>> ans;
+        vector<int> part;
+        solve(arr, target, n, ans, part, 0, 0);
         return ans;
     }
 };
