dcf5f441ab930fcc39c3467b05f877a7a9b3b150
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-07-12 18:14:52 +0530

    Time: 0 ms (100%), Space: 12.4 MB (95.99%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& arr) {
-        int n=arr.size();
-        if(n==1)return 0;
-        if(arr[0]>arr[1])return 0;
-        if(arr[n-1]>arr[n-2])return n-1;
-        int low=1,high=n-2;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(arr[mid]>arr[mid-1]&&arr[mid]>arr[mid+1])return mid;
-            else if(arr[mid]>arr[mid-1]){
-                low=mid+1;
-            }else {
-                high=mid-1;
+        int n = arr.size();
+
+        int ans = -1;
+        int start = 0;
+        int end = n - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (n == 1)
+                return 0;
+            if (mid == 0) {
+                if (arr[0] > arr[1])
+                    return 0;
+                else
+                    return 1;
             }
 
+             if (mid == n - 1) {
+                if (arr[n - 1] > arr[n - 2]) return n - 1;
+                else return n - 2;
+            }
+
+            if (arr[mid] >= arr[mid - 1] && arr[mid] >= arr[mid + 1]) {
+                return mid;
+            }
+            else if (arr[mid - 1] <= arr[mid] && arr[mid] <= arr[mid + 1]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
         }
         return -1;
-
-
-        
     }
 };
