2736aa3d690e1a5499d6fd9dfb70d3a4231bdc1e
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-07-12 17:05:22 +0530

    Time: 0 ms (100%), Space: 17.4 MB (89.75%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,46 +1,46 @@
 class Solution {
 public:
-    int solve1(vector<int>& arr, int target, int n) {
-        int start=0,end=n-1;
+    int leftBound(vector<int>& arr, int target) {
+        int start = 0;
+        int end = arr.size() - 1;
         int ans=-1;
         while (start <= end) {
             int mid = (start + end) / 2;
             if (arr[mid] == target) {
-                ans = mid;
+                ans=mid;
                 end = mid - 1;
-            } else if (arr[mid] > target) {
-                end=mid-1;
+            }
+            else if (arr[mid] < target) {
+                start = mid + 1;
             } else {
-               start=mid+1;
+                end = mid - 1;
             }
         }
         return ans;
     }
-
-    int solve2(vector<int>& arr, int target, int n) {
-        int start=0,end=n-1;
-        int ans=-1;
+    int rightBound(vector<int>& arr, int target) {
+        int start = 0;
+        int end = arr.size() - 1;
+        int ans1=-1;
         while (start <= end) {
             int mid = (start + end) / 2;
             if (arr[mid] == target) {
-                ans = mid;
+                ans1=mid;
+                start = mid + 1;
+            }
+            else if (arr[mid] < target) {
                 start=mid+1;
-            } else if (arr[mid] > target) {
-                end=mid-1;
             } else {
-                start=mid+1;
+                end = mid - 1;
             }
         }
-        return ans;
+        return ans1;
     }
 
-    vector<int> searchRange(vector<int>& arr, int target) {
-        int n = arr.size();
-        int fistoccur = solve1(arr, target, n);
-        int secondocuur = solve2(arr, target, n);
-        vector<int>ans;
-        ans.push_back(fistoccur);
-        ans.push_back(secondocuur);
-        return ans;
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int leftAns=leftBound(nums,target);
+        int rightAns=rightBound(nums,target);
+        return {leftAns,rightAns};
+        
     }
 };
