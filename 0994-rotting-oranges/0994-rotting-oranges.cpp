f10f16407e01958f0dc489979f7ce1e6a7141bef
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-07-18 16:21:55 +0530

    Time: 3 ms (26.49%), Space: 17.3 MB (17.32%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,62 +1,53 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int ans=0;
-        vector<vector<bool>>vis(n,vector<bool>(m,false));
-        queue<pair<pair<int,int>, int>> q;  // Fixed: {{i,j}, time}
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    q.push({{i,j},0});
-                    vis[i][j] = true;  // Mark initial rotten
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<pair<int, pair<int, int>>> q;
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
+
+       
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {
+                    vis[i][j] = true;
+                    q.push({0, {i, j}});
                 }
             }
         }
-        //bfs
-        while(q.size()>0){
-            int i=q.front().first.first;
-            int j=q.front().first.second;
-            int time=q.front().second;
+        int ans = 0;
+        while (!q.empty()) {
+            int time = q.front().first;
+            int i = q.front().second.first;
+            int j = q.front().second.second;
             q.pop();
-            ans=max(ans,time);
 
-            if(i-1 >=0 && !vis[i-1][j] && grid[i-1][j]==1){
-                q.push({{i-1,j},time+1});
-                vis[i-1][j]=true;
-            }
+            ans = max(ans, time);
 
-            
-            if(j+1 < m && !vis[i][j+1] && grid[i][j+1]==1){
-                q.push({{i,j+1},time+1});
-                vis[i][j+1]=true;
-            }
+            int delI[] = {-1, +1, 0, 0};
+            int delJ[] = {0, 0, +1, -1};
 
-            
-            if(i+1 < n && !vis[i+1][j] && grid[i+1][j]==1){
-                q.push({{i+1,j},time+1});
-                vis[i+1][j]=true;
-            }
+            for (int k = 0; k < 4; k++) {
+                int newRow = i + delI[k];
+                int newCol = j + delJ[k];
 
-            
-            if(j-1 >=0 && !vis[i][j-1] && grid[i][j-1]==1){
-                q.push({{i,j-1},time+1});
-                vis[i][j-1]=true;
-            }
-
-        }
-
-        //check for fresh orange
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1 && !vis[i][j]){
-                    return -1;
+                if (newRow >= 0 && newCol >= 0 && newRow < n && newCol < m) {
+                    if (grid[newRow][newCol] == 1 &&
+                        vis[newRow][newCol] == false) {
+                        vis[newRow][newCol] = true;
+                        grid[newRow][newCol] = 2;
+                        q.push({time + 1, {newRow, newCol}});
+                    }
                 }
             }
         }
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1)
+                    return -1;
+            }
+        }
         return ans;
     }
 };
