8bee6cadfb786a27d6ef0ebbd13588a9cce62f99
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-07-19 18:23:31 +0530

    Time: 22 ms (84.15%), Space: 109.3 MB (56.71%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
-    int result=-1;
+    int result = -1;
 
-    void dfs(vector<int>&edges,int n,vector<bool>&vis,vector<int>&count,vector<int>&inRecursion,int startNode){
-        vis[startNode]=true;
-        inRecursion[startNode]=true;
+    void dfs(vector<int>& edges, int n, vector<bool>& vis, vector<int>& count,
+             vector<bool>& inRecPath, int startNode) {
+        vis[startNode] = true;
+        inRecPath[startNode] = true;
 
-        if(startNode!=-1 && edges[startNode]!=-1){
-            if(vis[edges[startNode]]==false){
-                count[edges[startNode]]=count[startNode]+1;
-                dfs(edges,n,vis,count,inRecursion,edges[startNode]);
-            }else if(inRecursion[edges[startNode]]){
+        if (edges[startNode] != -1) {
+            if (vis[edges[startNode]] == false) {
+                count[edges[startNode]] = count[startNode] + 1;
+                dfs(edges,n,vis,count,inRecPath,edges[startNode]);
+            }else if(inRecPath[edges[startNode]]==true){
                 result=max(result,count[startNode]-count[edges[startNode]]+1);
             }
         }
-        inRecursion[startNode]=false;
+        inRecPath[startNode]=false;
+
     }
+
     int longestCycle(vector<int>& edges) {
-        int n=edges.size();
+        int n = edges.size();
+        vector<bool> vis(n, false);
+        vector<int> count(n, 1);
+        vector<bool> inRecuPath(n, false);
 
-        vector<bool>vis(n,false);
-
-        vector<int>count(n,1);
-
-        vector<int>inRecursion(n,false);
-
-        for(int i=0;i<n;i++){
-            if(vis[i]==false){
-                dfs(edges,n,vis,count,inRecursion,i);
+        for (int i = 0; i < edges.size(); i++) {
+            if (vis[i] == false) {
+                dfs(edges, n, vis, count, inRecuPath, i);
             }
         }
-
         return result;
     }
 };
