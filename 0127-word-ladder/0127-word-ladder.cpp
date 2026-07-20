7ab4be1e8f0c6b92e13962349718f94a04e4d2d1
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-07-20 15:34:40 +0530

    Time: 54 ms (68.33%), Space: 21.2 MB (67.76%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> wordSet(wordList.begin(), wordList.end());
-        if (wordSet.find(endWord) == wordSet.end()) return 0;
-        
+    int ladderLength(string beginWord, string endWord,
+                     vector<string>& wordList) {
+
+        unordered_set<string> st(wordList.begin(), wordList.end());
+
+        if (st.find(endWord) == st.end())
+            return 0;
+
         queue<pair<string, int>> q;
         q.push({beginWord, 1});
-        unordered_set<string> visited;
-        visited.insert(beginWord);
-        
+        st.erase(beginWord);
+
         while (!q.empty()) {
-            auto [current, steps] = q.front();
+            string word = q.front().first;
+            int len = q.front().second;
             q.pop();
-            
-            if (current == endWord) return steps;
-            
-            for (int i = 0; i < current.size(); ++i) {
-                char original = current[i];
-                for (char c = 'a'; c <= 'z'; ++c) {
-                    if (c == original) continue;
-                    current[i] = c;
-                    if (wordSet.find(current) != wordSet.end() && visited.find(current) == visited.end()) {
-                        if (current == endWord) return steps + 1;
-                        visited.insert(current);
-                        q.push({current, steps + 1});
+
+            if (word == endWord)
+                return len;
+
+            for (int i = 0; i < word.size(); i++) {
+                char original = word[i];
+
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    word[i] = ch;
+
+                    if (st.find(word) != st.end()) {
+                        st.erase(word);
+                        q.push({word, len + 1});
                     }
                 }
-                current[i] = original;
+
+                word[i] = original;
             }
         }
+
         return 0;
     }
 };
