4741b9e3e91439c50d4527249126d18adcb0efcc
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-07-22 20:58:54 +0530

    Time: 24 ms (78.9%), Space: 107.5 MB (65.99%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-    int result=-1;
-    void solve(vector<int>&edges,int n,vector<bool>&vis,vector<int>&count,vector<bool>&inRecPath,int i){
-        vis[i]=true;
-        inRecPath[i]=true;
-
-        if(edges[i]!=-1){
-            if(vis[edges[i]]==false){
-                //not visited
-                count[edges[i]]=count[i]+1;
-                solve(edges,n,vis,count,inRecPath,edges[i]);
-            }else if(inRecPath[edges[i]]==true){
-                //cycle mil gayi
-                int temp=count[i]-count[edges[i]]+1;
-                result=max(result,temp);
+    int result = -1;
+    void dfs(int n, vector<int>& edges, vector<bool>& vis, vector<int>& count,
+             vector<bool>& isRecPath, int i) {
+        vis[i] = true;
+        isRecPath[i] = true;
+        if (edges[i] != -1) {
+            if (vis[edges[i]] == false) {
+                count[edges[i]] = count[i] + 1;
+                dfs(n, edges, vis, count, isRecPath, edges[i]);
+            } else if (isRecPath[edges[i]] == true) {
+                // cycle mil gayi
+                int len = count[i] - count[edges[i]] + 1;
+                result = max(result, len);
             }
-        }
-        inRecPath[i]=false;
-    }
-    int longestCycle(vector<int>& edges) {
-        int n=edges.size();
-        vector<bool>vis(n,false);
-        vector<int>count(n,1);
-        vector<bool>inRecPath(n,false);
 
-        for(int i=0;i<n;i++){
-            if(edges[i]==-1)continue;
-            if(vis[i]==false)
-                solve(edges,n,vis,count,inRecPath,i);
+           
+        }
+         isRecPath[i] = false;
+    }
+    int longestCycle(vector<int>& Edge) {
+        int n = Edge.size();
+        vector<bool> vis(n, false);
+        vector<int> count(n, 1);
+        vector<bool> isRecPath(n, false);
+        for (int i = 0; i < n; i++) {
+            if (Edge[i] == -1)
+                continue;
+            if (vis[i] == false) {
+                dfs(n, Edge, vis, count, isRecPath, i);
+            }
         }
         return result;
     }
-    
 };
