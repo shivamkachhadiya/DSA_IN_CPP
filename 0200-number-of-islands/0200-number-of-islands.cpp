52ae279de2478ba8feb16084398110d06cfee47b
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-07-23 22:23:00 +0530

    Time: 30 ms (34.01%), Space: 18.6 MB (37.94%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     queue<pair<int, int>> q;
 
-    void bfs(vector<vector<char>>& grid, int n, int m,
-             vector<vector<bool>>& vis, int i, int j) {
+    void bfs(vector<vector<char>>& grid, int n, int m,vector<vector<bool>>& vis, int i, int j) {
         int delR[] = {-1, 1, 0, 0};
         int delC[] = {0, 0, -1, 1};
 
-        for (int k = 0; k < 4; k++)
+      
             while (!q.empty()) {
                 int row = q.front().first;
                 int col = q.front().second;
@@ -33,7 +32,6 @@ public:
             for (int j = 0; j < m; j++) {
                 if (vis[i][j] == false && grid[i][j] == '1') {
                     q.push({i, j});
-
                     vis[i][j] = true;
                     bfs(grid, n, m, vis, i, j);
                     isLands++;
