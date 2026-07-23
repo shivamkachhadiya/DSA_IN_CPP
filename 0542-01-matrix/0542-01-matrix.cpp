e38ce2ff48d43b936cdb7acc2c1fad574191b6c3
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-07-23 22:31:57 +0530

    Time: 26 ms (29.87%), Space: 38.1 MB (43.37%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -4,43 +4,41 @@ public:
         int n = mat.size();
         int m = mat[0].size();
         vector<vector<int>> temp_grid = mat;
-        queue<pair<pair<int, int>,int>> q;
+        queue<pair<pair<int, int>, int>> q;
         vector<vector<bool>> vis(n, vector<bool>(m, false));
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (mat[i][j] == 0) {
                     vis[i][j] = true;
-                    // temp_grid[i][j]=1;
-                    q.push({{i,j},0});
-                }
+                    q.push({{i, j}, 0});
                 }
             }
-            int ans = -1;
-            while (!q.empty()) {
-                int i = q.front().first.first;
-                int j = q.front().first.second;
-                int time = q.front().second;
-                q.pop();
+        }
 
-                temp_grid[i][j] = time;
+        while (!q.empty()) {
+            int i = q.front().first.first;
+            int j = q.front().first.second;
+            int time = q.front().second;
+            q.pop();
 
-                int delR[] = {-1, +1, 0, 0};
-                int delC[] = {0, 0, -1, +1};
+            temp_grid[i][j] = time;
 
-                for (int k = 0; k < 4; k++) {
-                    int delRow = delR[k] + i;
-                    int delCol = delC[k] + j;
+            int delR[] = {-1, +1, 0, 0};
+            int delC[] = {0, 0, -1, +1};
 
-                    if (delRow >= 0 && delCol >= 0 && delRow < n &&
-                        delCol < m) {
-                        if (vis[delRow][delCol] == false) {
-                            vis[delRow][delCol] = true;
-                            q.push({{delRow,delCol},time+1});
-                        }
+            for (int k = 0; k < 4; k++) {
+                int delRow = delR[k] + i;
+                int delCol = delC[k] + j;
+
+                if (delRow >= 0 && delCol >= 0 && delRow < n && delCol < m) {
+                    if (vis[delRow][delCol] == false) {
+                        vis[delRow][delCol] = true;
+                        q.push({{delRow, delCol}, time + 1});
                     }
                 }
             }
-            return temp_grid;
         }
-    };
+        return temp_grid;
+    }
+};
