a8b857c4a48bb929b03eb57068eb15b54f9966ee
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-07-23 17:12:49 +0530

    Time: 74 ms (87.43%), Space: 44.5 MB (53.29%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -3,40 +3,37 @@ public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<vector<pair<int, int>>> adj(n + 1);
         for (int i = 0; i < times.size(); i++) {
-            int source = times[i][0];
-            int desti = times[i][1];
-            int time = times[i][2];
-
-            adj[source].push_back({desti, time});
+            int u = times[i][0];
+            int v = times[i][1];
+            int w = times[i][2];
+            adj[u].push_back({v, w});
         }
 
+        vector<int> dist_arr(n + 1, 1e9);
         priority_queue<pair<int, int>, vector<pair<int, int>>,
                        greater<pair<int, int>>>
-            pq;
-        pq.push({0, k});
-        vector<int> dest_arr(n + 1, 1e9);
-        dest_arr[k] = 0;
-
-        while (!pq.empty()) {
-            int dist = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
+            q;
+        q.push({0, k});
+        dist_arr[k] = 0;
+        while (!q.empty()) {
+            int dist = q.top().first;
+            int node = q.top().second;
+            q.pop();
             for (auto& x : adj[node]) {
                 int newNode = x.first;
-                int newDist = x.second;
-                int total_dist = dist + newDist;
-                if (total_dist < dest_arr[newNode]) {
-                    dest_arr[newNode] = total_dist;
-                    pq.push({total_dist, newNode});
+                int newDist = dist + x.second;
+                if (newDist < dist_arr[newNode]) {
+                    dist_arr[newNode] = newDist;
+                    q.push({newDist, newNode});
                 }
             }
         }
-        int ans = 0;
-        for (int i = 1; i <= n; i++) {
-            if (dest_arr[i] == 1e9)
-                return -1;
-            ans = max(ans, dest_arr[i]);
-        }
+
+        int ans = *max_element(dist_arr.begin() + 1, dist_arr.end());
+
+        if (ans == 1e9)
+            return -1;
+
         return ans;
     }
 };
