8b6685ff15018b704e681ba2ad848206d2fd7756
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-07-24 22:50:59 +0530

    Time: 83 ms (57.38%), Space: 44.2 MB (82.85%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int, int>>> adj(n + 1);
-        for (int i = 0; i < times.size(); i++) {
-            int u = times[i][0];
-            int v = times[i][1];
-            int w = times[i][2];
-            adj[u].push_back({v, w});
+        vector<vector<pair<int, int>>> adj(n+1);
+        int size = times.size();
+        for (int i = 0; i < size; i++) {
+            int source = times[i][0];
+            int destination = times[i][1];
+            int time = times[i][2];
+            adj[source].push_back({destination, time});
         }
-
-        vector<int> dist_arr(n + 1, 1e9);
+        vector<int> dist_vec(n+1, 1e9);
+        dist_vec[k] = 0;
         priority_queue<pair<int, int>, vector<pair<int, int>>,
                        greater<pair<int, int>>>
-            q;
-        q.push({0, k});
-        dist_arr[k] = 0;
-        while (!q.empty()) {
-            int dist = q.top().first;
-            int node = q.top().second;
-            q.pop();
+            pq;
+        pq.push({0, k});
+
+        while (!pq.empty()) {
+            int dist = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
             for (auto& x : adj[node]) {
                 int newNode = x.first;
                 int newDist = dist + x.second;
-                if (newDist < dist_arr[newNode]) {
-                    dist_arr[newNode] = newDist;
-                    q.push({newDist, newNode});
+               
+                if (newDist < dist_vec[newNode]) {
+                    dist_vec[newNode] = newDist;
+                    pq.push({newDist, newNode});
                 }
             }
         }
-
-        int ans = *max_element(dist_arr.begin() + 1, dist_arr.end());
-
-        if (ans == 1e9)
-            return -1;
-
+        int ans=*max_element(dist_vec.begin()+1,dist_vec.end());
+        if(ans==1e9)return -1;
         return ans;
     }
 };
