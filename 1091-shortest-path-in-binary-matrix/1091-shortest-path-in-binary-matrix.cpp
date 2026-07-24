e01e893cbcb8ba743e9f10e2cb428528e1ceeef3
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-07-24 22:13:09 +0530

    Time: 19 ms (53.33%), Space: 24.8 MB (66.74%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        if (grid[0][0] == 1)
-            return -1;
-
         int n = grid.size();
         int m = grid[0].size();
-        if (grid[n - 1][m - 1] == 1)
-            return -1;
+        if(grid[0][0]==1)return -1;
         queue<pair<pair<int, int>, int>> q;
         q.push({{0, 0}, 0});
-
-        int delR[] = {0, 1, 0, -1, -1, 1, 1, -1};
-        int delC[] = {-1, 1, 1, 1, 0, -1, 0, -1};
         vector<vector<bool>> vis(n, vector<bool>(m, false));
-        int ans = -1;
         vis[0][0] = true;
+        int delR[] = {0, 1, 0, 1, -1, -1, 1, -1};
+        int delC[] = {-1, -1, 1, 1, 0, 1, 0, -1};
         while (!q.empty()) {
             int i = q.front().first.first;
             int j = q.front().first.second;
             int time = q.front().second;
-            if (i == n - 1 && j == m - 1)
-                return time + 1;
-
             q.pop();
+            if (i == n - 1 && j == m - 1)
+                return time+1;
             for (int k = 0; k < 8; k++) {
-                int newRow = i + delR[k];
-                int newCol = j + delC[k];
-
-                if (newRow >= 0 && newCol >= 0 && newRow < n && newCol < m &&
-                    vis[newRow][newCol] == false && grid[newRow][newCol] == 0) {
-                    vis[newRow][newCol] = true;
-                    q.push({{newRow, newCol}, time + 1});
+                int delRow = i + delR[k];
+                int delCol = j + delC[k];
+                if (delRow >= 0 && delCol >= 0 && delRow < n && delCol < m) {
+                    if (grid[delRow][delCol] != 1 &&
+                        vis[delRow][delCol] == false) {
+                        vis[delRow][delCol] = true;
+                        q.push({{delRow, delCol}, time + 1});
+                    }
                 }
             }
         }
+
         return -1;
     }
 };
