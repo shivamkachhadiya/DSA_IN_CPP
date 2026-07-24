1964fc603a1542eb273c764c5b6334a30f55aff2
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-07-24 17:10:56 +0530

    Time: 8 ms (82.91%), Space: 35 MB (54.27%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
-    bool isValid(vector<int>&arr,int days,int max_allowed_capacity){
-        int local_days=1;
-        int cap=0;
-        for(int i=0;i<arr.size();i++){
-             if(arr[i] > max_allowed_capacity)
-        return false;
-            cap+=arr[i];
-            if(cap>max_allowed_capacity){
-                local_days++;
-                cap=arr[i];
+    bool isValid(vector<int>& arr, int n, int d, int capacity) {
+        int last_sum = arr[0];
+        int days = 1;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > capacity)
+                return false;
+            last_sum += arr[i];
+            if (last_sum > capacity) {
+                days++;
+                last_sum = arr[i];
             }
         }
-        if(local_days<=days){
+        if (days <= d) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
-    int shipWithinDays(vector<int>& arr, int days) {
-        int n=arr.size();
-        int sum=0;
-        for(auto &x:arr){
-            sum+=x;
+    int shipWithinDays(vector<int>& arr, int d) {
+        int n = arr.size();
+        
+        int start = *max_element(arr.begin(),arr.end());
+        int sum = 0;
+        for (auto& x : arr) {
+            sum += x;
         }
-        int end=sum;
-        int start=1;
-        int ans=0;
-        while(start<=end){
-            int mid=(start+end)/2;
-            if(isValid(arr,days,mid)){
-                ans=mid;
-                end=mid-1;
-            }else{
-                start=mid+1;
+        int end = sum;
+        int ans = 0;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (isValid(arr, n, d, mid)) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
         }
         return ans;
