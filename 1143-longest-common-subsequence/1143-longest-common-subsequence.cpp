cb1141d39daabacaed96c3d204570110af27ebd1
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-04 22:31:35 +0530

    Time: 76 ms (5.16%), Space: 27.7 MB (16.85%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-    int solve(string text1, string text2, int n1, int n2, int i, int j,
-              vector<vector<int>>& dp) {
-        if (i >= n1 || j >= n2)
-            return 0;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        if (text1[i] == text2[j]) {
-            return dp[i][j] = 1 + solve(text1, text2, n1, n2, i + 1, j + 1, dp);
+    int solve(string &t1,string &t2,int n1,int n2,int i,int j,vector<vector<int>>&dp){
+       if(i>=n1 || j>=n2)return 0;
+       if(dp[i][j]!=-1)return dp[i][j];
+        int take=0;
+        if(t1[i]==t2[j]){
+            take=1+solve(t1,t2,n1,n2,i+1,j+1,dp);
         }
-        return dp[i][j] = max(solve(text1, text2, n1, n2, i + 1, j, dp),
-                              solve(text1, text2, n1, n2, i, j + 1, dp));
+        int notake= max(solve(t1,t2,n1,n2,i+1,j,dp),solve(t1,t2,n1,n2,i,j+1,dp));
+        return dp[i][j]=max(take,notake);
+        
     }
     int longestCommonSubsequence(string text1, string text2) {
-        int n1 = text1.size();
-        int n2 = text2.size();
-        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
-        // return solve(text1,text2,n1,n2,0,0,dp);
-        for (int i = n1 - 1; i >= 0; i--) {
-            for (int j = n2 - 1; j >= 0; j--) {
-                if (text1[i] == text2[j]) {
-                    dp[i][j] = 1 + dp[i + 1][j + 1];
-                    continue;
-                }
-                dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]);
-            }
-        }
-        return dp[0][0]+1;//becouse dp init with -1
+        int n1=text1.size();
+        int n2=text2.size();
+        vector<vector<int>>dp(n1,vector<int>(n2,-1));
+        return solve(text1,text2,n1,n2,0,0,dp);
     }
 };
