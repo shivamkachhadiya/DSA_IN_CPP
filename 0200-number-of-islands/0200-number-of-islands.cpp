e008be37f7a81d0f363423336533ea13a476f848
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-06 17:23:45 +0530

    Time: 32 ms (23.36%), Space: 16.8 MB (61.26%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,43 +1,32 @@
 class Solution {
 public:
-    queue<pair<int, int>> q;
-
-    void bfs(vector<vector<char>>& grid, int n, int m,vector<vector<bool>>& vis, int i, int j) {
-        int delR[] = {-1, 1, 0, 0};
-        int delC[] = {0, 0, -1, 1};
-
-      
-            while (!q.empty()) {
-                int row = q.front().first;
-                int col = q.front().second;
-                q.pop();
-                for (int k = 0; k < 4; k++) {
-                    int newRow = row + delR[k];
-                    int newCol = col + delC[k];
-                    if (newRow >= 0 && newCol >= 0 && newRow < n &&
-                        newCol < m && grid[newRow][newCol] == '1' &&
-                        vis[newRow][newCol] == false) {
-                        vis[newRow][newCol] = true;
-                        q.push({newRow, newCol});
-                    }
-                }
+    int delRow[4] = {-1, 1, 0, 0};
+    int delCol[4] = {0, 0, -1, 1};
+    void dfs(vector<vector<char>>& grid, int n, int m,
+             vector<vector<bool>>& vis, int i, int j) {
+        vis[i][j] = true;
+        for (int k = 0; k < 4; k++) {
+            int newRow = i + delRow[k];
+            int newCol = j + delCol[k];
+            if (newRow >= 0 && newCol >= 0 && newRow < n && newCol < m &&
+                vis[newRow][newCol] == false && grid[newRow][newCol] == '1') {
+                dfs(grid, n, m, vis, newRow, newCol);
             }
+        }
     }
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
+        int island = 0;
         vector<vector<bool>> vis(n, vector<bool>(m, false));
-        int isLands = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (vis[i][j] == false && grid[i][j] == '1') {
-                    q.push({i, j});
-                    vis[i][j] = true;
-                    bfs(grid, n, m, vis, i, j);
-                    isLands++;
+                    dfs(grid, n, m, vis, i, j);
+                    island++;
                 }
             }
         }
-        return isLands;
+        return island;
     }
 };
