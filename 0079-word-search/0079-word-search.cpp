23ef337fb6d27b23ee7118bfdde741b017b4cd2d
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-07 21:46:43 +0530

    Time: 823 ms (21.68%), Space: 11.5 MB (37.3%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool solve(int n,int m,vector<vector<char>>&mat,int i,int j,string word,
+                int idx){
+        if(idx==word.size())return true;
+        if(i<0||j<0||i>=n||j>=m||mat[i][j]=='$')return false;
+        if(mat[i][j]!=word[idx])return false;
+        char temp=mat[i][j];
+        mat[i][j]='$';
+//{
+        //furthr call up left right down
+        bool found = solve(n,m,mat,i+1,j,word,idx+1)||
+        solve(n,m,mat,i,j+1,word,idx+1)||
+        solve(n,m,mat,i-1,j,word,idx+1)||
+        solve(n,m,mat,i,j-1,word,idx+1);
+
+
+//}
+        mat[i][j]=temp;
+        return found;
+    }
+    bool exist(vector<vector<char>>& mat, string word) {
+        int n=mat.size();
+        int m=mat[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==word[0]&&solve(n,m,mat,i,j,word,0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
