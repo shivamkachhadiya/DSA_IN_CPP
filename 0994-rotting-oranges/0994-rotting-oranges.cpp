629f020b43dd0be401e149bc6bba584ddf502ae7
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-07 22:55:48 +0530

    Time: 0 ms (100%), Space: 17.1 MB (28.67%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,53 +1,44 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-
-        queue<pair<pair<int, int>, int>> q;
-        int fresh = 0;
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1)
-                    fresh++;
-                if (grid[i][j] == 2) {
-                    vis[i][j] = true;
-                    grid[i][j] = 0;
-
-                    q.push({{i, j}, 0});
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<pair<int,int>,int>>q;
+        vector<vector<int>>vis(n,vector<int>(m,false));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    vis[i][j]=true;
+                    q.push({{i,j},0});
                 }
             }
         }
-        int delRow[] = {1, 0, -1, 0};
-        int delCol[] = {0, 1, 0, -1};
-        int ans = -1;
-        if (fresh > 0 && q.empty())
-            return -1;
-        if (fresh == 0)
-            return 0;
-        while (!q.empty()) {
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int time = q.front().second;
-            ans = max(ans, time);
-
+        int delRow[]={-1,1,0,0,};
+        int delCol[]={0,0,1,-1};
+        int ans=0;
+        while(!q.empty()){
+            int row=q.front().first.first;
+            int col=q.front().first.second;
+            int time=q.front().second;
             q.pop();
-            for (int k = 0; k < 4; k++) {
-                int newRow = delRow[k] + row;
-                int newCol = delCol[k] + col;
-                if (newRow >= 0 && newCol >= 0 && newRow < n && newCol < m &&
-                    vis[newRow][newCol] == false && grid[newRow][newCol] == 1) {
-                    vis[newRow][newCol] = true;
-                    grid[newRow][newCol] = 0;
-                    q.push({{newRow, newCol}, time + 1});
+            ans=time;
+            for(int k=0;k<4;k++){
+                int newRow=row+delRow[k];
+                int newCol=col+delCol[k];
+                if(newRow>=0&&newCol>=0&&newRow<n&&newCol<m&&
+                    vis[newRow][newCol]==false&&grid[newRow][newCol]==1){
+                    vis[newRow][newCol]=true;
+                    grid[newRow][newCol]=2;
+                    q.push({{newRow,newCol},time+1});
+                    
                 }
             }
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1)
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
                     return -1;
+                }
             }
         }
         return ans;
