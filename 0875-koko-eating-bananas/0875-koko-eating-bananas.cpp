5f9618d1c0b9c6f5fba31240a9ccf19e95d59dd8
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-08 17:00:59 +0530

    Time: 15 ms (17.43%), Space: 22.9 MB (54.56%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    bool isValid(vector<int>& arr, int h, int k) {
-        long long sum = 0;
+    bool isValid(vector<int>& arr, int mid, int h) {
+        long long take_time = 0;
         for (int i = 0; i < arr.size(); i++) {
-            long long c_val = ceil((double)arr[i] / k);
-            sum += c_val;
+            take_time += ceil((double)arr[i] / mid);
         }
-        if (sum <= h) {
-            return true;
-        } else {
-            return false;
-        }
+        return take_time <= h;
     }
-    int minEatingSpeed(vector<int>& piles, int h) {
+    int minEatingSpeed(vector<int>& arr, int h) {
+        int n = arr.size();
         int start = 1;
-        int end = *max_element(piles.begin(), piles.end());
-        int ans = -1;
+        int end = *max_element(arr.begin(), arr.end());
+        int ans = end;
+
         while (start <= end) {
             int mid = (start + end) / 2;
-            if (isValid(piles, h, mid)) {
+            if (isValid(arr, mid, h)) {
                 ans = mid;
                 end = mid - 1;
             } else {
