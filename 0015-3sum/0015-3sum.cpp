1f3bdf2377691164fe42d902d03ff1e984efbe97
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-08 15:54:51 +0530

    Time: 39 ms (94.87%), Space: 29.2 MB (47.22%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
+    // 2 sum  n1+n2=target
+    // 3 sum n1+n2+n3=target
+    // convert n2+n3=0-n1
     vector<vector<int>> threeSum(vector<int>& arr) {
-        sort(arr.begin(), arr.end()); // MISTAKE: array must be sorted first
-
+        sort(arr.begin(), arr.end());
+        vector<vector<int>> ans;
         int n = arr.size();
-        vector<vector<int>>
-            result; // MISTAKE 1: Fixed variable name from "ans" to "result"
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && arr[i] == arr[i - 1])
-                continue; // MISTAKE 2: Changed i==0 to i>0 (avoid i-1=-1) int
-                          // left = i + 1;
-            int left = i + 1;
+        for (int i = 0; i < n; i++) {
+            int n1 = arr[i];
+            int start = i + 1;
+            int end = n - 1;
+            int target = -n1;
 
-            int right = n - 1;
-            int sum = -1 * arr[i];
-            while (left < right) {
-                int s = arr[left] + arr[right];
-                if (s == sum) {
-                    // MISTAKE 3: Fixed push_back() left++;
-                    result.push_back({arr[i], arr[left], arr[right]});
-                    left++;
-                    right--;
-                    while (left < right && arr[left] == arr[left - 1]) {
-                        left++;
-                    }
-                    while (right > left && arr[right] == arr[right + 1]) {
-                        right--;
-                    }
-                } else if (s < sum) {
-                    left++;
-                } else {
-                    right--;
+            while (start < end) {  
+                int sum=arr[start]+arr[end];
+                if (sum == target) {
+                    int n2 = arr[start];
+                    int n3 = arr[end];
+                    ans.push_back({n1, n2, n3});
+                    while(start<end&&arr[start]==arr[start+1])start++;
+                    while(start<end&&arr[end]==arr[end-1])end--;
+                    start++;
+                    end--;                
+                }else if(sum>target){
+                    end--;
+                }else{
+                    start++;
                 }
             }
+            while(i+1<n&&arr[i]==arr[i+1])i++;
         }
-        return result;
+        return ans;
     }
 };
