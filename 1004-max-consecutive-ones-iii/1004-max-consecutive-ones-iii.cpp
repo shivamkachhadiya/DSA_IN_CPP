4e4fdd63218c027362b39862d2dbcafe0d8954e4
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-10 14:32:41 +0530

    Time: 2 ms (37.03%), Space: 69.5 MB (95.95%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
-    int longestOnes(vector<int>& nums, int k) {
-        int n = nums.size();
-        int left = 0, right = 0;
+    int longestOnes(vector<int>& arr, int k) {
+        int n = arr.size();
+        int maxi = 0;
+        int left=0;
         int zeros = 0;
-        int maxLength = 0;
-        while (right < n) {
-            if (nums[right] == 1) {
-                int currLength = right - left + 1;
-                maxLength = max(maxLength, currLength);
-                right++;
-            } else if (nums[right] == 0) {
+        for (int right = 0; right < n; right++) {
+            if (arr[right] == 0) {
                 zeros++;
-                if (zeros <= k) {
-                    int currLength = right - left + 1;
-                    maxLength = max(maxLength, currLength);
-                    right++;
-                } else {
-                    while (zeros != k) {
-                        if (nums[left] == 0) {
-                            zeros--;
-                            left++;
-                        } else {
-                            left++;
-                        }
-                    }
-                    int currLength = right - left + 1;
-                    maxLength = max(maxLength, currLength);
-                    right++;
-                }
             }
+            while (zeros > k) {
+                if (arr[left] == 0) {
+                    zeros--;
+                }
+                left++;
+            }
+            maxi = max(maxi, right - left + 1);
         }
-        return maxLength;
+
+        return maxi;
     }
 };
