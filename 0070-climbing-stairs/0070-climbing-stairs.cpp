bfee7c3f710b34fcfd46b13c1eff2701f8ee583a
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-10 15:03:13 +0530

    Time: 0 ms (100%), Space: 8.6 MB (21.01%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    int solve(int n, int i, vector<int>& dp) {
-        if (i == n) return 1;       // reached exactly top → valid way
-        if (i > n) return 0;        // overshoot → invalid
-        if (dp[i] != -1) return dp[i];
+    int solve(int n,int i,vector<int>&dp){
+        if(i>n)return 0;
+        if(i==n)return 1;
+        if(dp[i]!=-1)return dp[i];
+        int takeonestep=solve(n,i+1,dp);
+        int taketwosteps=solve(n,i+2,dp);
+        return dp[i]=takeonestep+taketwosteps;
 
-        int one_way = solve(n, i + 1, dp);
-        int two_way = solve(n, i + 2, dp);
-
-        return dp[i] = one_way + two_way;
     }
-
     int climbStairs(int n) {
-        vector<int> dp(n + 1, -1);
-        return solve(n, 0, dp);
+        vector<int>dp(n,-1);
+        return solve(n,0,dp);
     }
 };
