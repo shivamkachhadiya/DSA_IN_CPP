85fd737dcaeadc0b25aa62bccfb9eb3ab8ffefcb
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-11 11:51:31 +0530

    Time: 7 ms (5.4%), Space: 12 MB (5.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int n = s.length();
-        int start = 0;
-        int end = n - 1;
-        for (char& c : s) {
-            c = std::tolower(c);
+        stack<char> st;
+        string temp = "";
+        for (char& x : s) {
+            if (!isalnum(x))
+                continue; // skip non-alphanumeric            
+            temp.push_back(tolower(static_cast<unsigned char>(x)));
+            st.push(tolower(static_cast<unsigned char>(x)));
         }
-        while (start < end) {
-            if (!iswalnum(s[start])) {// check is alphanumeric or not iswalnum()
-                start++;
-                continue;
-            }
-            if (!iswalnum(s[end])) {
-                end--;
-                continue;
-            }
-            if (s[start] != s[end]) {
-                return false;
 
-            } else {
-                start++;
-                end--;
+        int i = 0;
+        int n = temp.size();
+        while (i < n) {
+             if (temp[i] != st.top()) {
+                return false;  // mismatch found
+            }
+            if (temp[i] == st.top()) {
+                st.pop();
+                i++;
             }
         }
-        return true;
+        return st.empty();
     }
 };
