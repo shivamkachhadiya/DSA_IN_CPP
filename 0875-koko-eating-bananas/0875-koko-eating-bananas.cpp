baa48383f348a5c84898a81738d36bf66efa7eb9
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-11 13:31:25 +0530

    Time: 10 ms (39.63%), Space: 22.9 MB (55.08%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    bool isValid(vector<int>& arr, int mid, int h) {
-        long long take_time = 0;
-        for (int i = 0; i < arr.size(); i++) {
-            take_time += ceil((double)arr[i] / mid);
+    bool isValid(vector<int>&arr,int mid,int h){
+        long long total_hours=0;
+        for(int i=0;i<arr.size();i++){
+            long long taken=ceil((double)arr[i]/mid);
+            total_hours+=taken;
         }
-        return take_time <= h;
+        return total_hours<=h;
     }
     int minEatingSpeed(vector<int>& arr, int h) {
-        int n = arr.size();
-        int start = 1;
-        int end = *max_element(arr.begin(), arr.end());
-        int ans = end;
-
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (isValid(arr, mid, h)) {
-                ans = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
+        int n=arr.size();
+        int ans=0;
+        int start=1;
+        int end=*max_element(arr.begin(),arr.end());
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(isValid(arr,mid,h)){
+                ans=mid;
+                end=mid-1;
+            }else{
+                start=mid+1;
             }
         }
         return ans;
