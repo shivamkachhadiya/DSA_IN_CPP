8bb5f52eec992fc17ddb5065fdccbf03c58298ae
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-11 12:22:40 +0530

    Time: 399 ms (15.53%), Space: 17.7 MB (33.72%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
+    int count=0;
+    bool isValid(string &str){
+        int start=0;
+        int end=str.size()-1;
+        while(start<end){
+            if(str[start]!=str[end])return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
     int countSubstrings(string s) {
-        int res = 0;
-
-        for (int i = 0; i < s.size(); i++) {
-            for (int j = i; j < s.size(); j++) {
-                int l = i, r = j;
-                while (l < r && s[l] == s[r]) {
-                    l++;
-                    r--;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            string temp="";
+            for(int j=i;j<n;j++){
+                temp.push_back(s[j]);
+                if(isValid(temp)){
+                    count++;
                 }
-                res += (l >= r);
             }
         }
-
-        return res;
+        return count;
     }
 };
