8dac033aa6f28f6c4e6b2c1aa22bd3b2709793bb
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-13 16:50:26 +0530

    Time: 0 ms (100%), Space: 8.6 MB (39.06%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -4,10 +4,7 @@ public:
         if(i>n)return 0;
         if(i==n)return 1;
         if(dp[i]!=-1)return dp[i];
-        int takeonestep=solve(n,i+1,dp);
-        int taketwosteps=solve(n,i+2,dp);
-        return dp[i]=takeonestep+taketwosteps;
-
+        return dp[i]=solve(n,i+1,dp)+solve(n,i+2,dp);
     }
     int climbStairs(int n) {
         vector<int>dp(n,-1);
