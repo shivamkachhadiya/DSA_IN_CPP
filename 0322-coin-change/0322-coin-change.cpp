769611bba33b764011fd1d0440482dd1de87774d
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-13 21:30:53 +0530

    Time: 38 ms (49.69%), Space: 19.2 MB (55.31%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
-    const int INF = 1e9;
-
-    int solve(vector<int>& coins, int i, int target, vector<vector<int>>& dp) {
-
-        // base case
-        if (target == 0) return 0;
-
-        if (i == 0) {
-            if (target % coins[0] == 0)
-                return target / coins[0];
-            else
-                return INF;
+    int solve(vector<int>&coins,int target,vector<int>&dp){
+        if(target==0){
+            return 0;
         }
+        if(target<0){
+            return INT_MAX;
+        }
+        if(dp[target]!=-1)return dp[target];
+        int mini=INT_MAX;
 
-        // memo check
-        if (dp[i][target] != -1)
-            return dp[i][target];
-
-        // not take current coin
-        int notake = solve(coins, i - 1, target, dp);
-
-        // take current coin (unbounded)
-        int take = INF;
-        if (coins[i] <= target)
-            take = 1 + solve(coins, i, target - coins[i], dp);
-
-        return dp[i][target] = min(take, notake);
+        for(int i=0;i<coins.size();i++){
+            int ans=solve(coins,target-coins[i],dp);
+            if(ans!=INT_MAX){
+                mini=min(mini,ans+1);
+            }
+        }
+        return dp[target]=mini;
     }
-
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-
-        int ans = solve(coins, n - 1, amount, dp);
-        return (ans >= INF) ? -1 : ans;
+        
+        int n=coins.size();
+        vector<int>dp(amount+1,-1);
+        int ans=solve(coins,amount,dp);
+        if(ans!=INT_MAX)return ans;
+        return -1;
     }
 };
