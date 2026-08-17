2ce5097466ec37841a5d4a938bb1e03e258eca67
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-17 12:35:45 +0530

    Time: 0 ms (100%), Space: 11.3 MB (17.13%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    void solve(vector<int>&arr,int n,vector<bool>&vis,vector<int>&part,vector<vector<int>>&ans){
-        if(part.size()==n){
-            ans.push_back(part);
+unordered_set<int>st;
+vector<vector<int>>ans;
+    void solve(vector<int>&arr,vector<int>&temp){
+        if(temp.size()==arr.size()){
+            ans.push_back(temp);
             return;
         }
-        for(int i=0;i<n;i++){
-            
-            if(vis[i]==true)continue;
-            //include
-            part.push_back(arr[i]);
-            vis[i]=true;
+        for(int i=0;i<arr.size();i++){
+            if(st.find(arr[i])==st.end()){
+                st.insert(arr[i]);
+                temp.push_back(arr[i]);
 
-            //recursive call
-            solve(arr,n,vis,part,ans);
+                solve(arr,temp);
 
-            //backtrack
-            part.pop_back();
-            vis[i]=false;
+                temp.pop_back();
+                st.erase(arr[i]);
+            }
         }
     }
+    
     vector<vector<int>> permute(vector<int>& arr) {
         int n=arr.size();
-        vector<bool>vis(n,false);
-        vector<vector<int>>ans;
-        vector<int>part;
-        solve(arr,n,vis,part,ans);
+        vector<int>temp;
+        solve(arr,temp);
         return ans;
-
     }
 };
