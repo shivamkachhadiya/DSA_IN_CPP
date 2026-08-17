90bc116519ba4e4578135a098dd3cdb584cfe56f
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-17 11:57:04 +0530

    Time: 1 ms (17.94%), Space: 28.1 MB (14.55%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -1,29 +1,18 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     int maxSum=INT_MIN;
-
     int solve(TreeNode* root){
         if(root==NULL)return 0;
-        int leftAns=solve(root->left);
-        int rightAns=solve(root->right);
+        int left=solve(root->left);
+        int right=solve(root->right);
 
-        int niche_hi_mil_gaya_ans=leftAns+rightAns+root->val;
-        int koi_ek_achha=max(leftAns,rightAns)+root->val;
+        int niche_hi_mil_gaya=left+right+root->val;
+        int koi_ek_achha=max(left,right)+root->val;
         int sirf_root_achha=root->val;
 
-        maxSum=max({maxSum,niche_hi_mil_gaya_ans,koi_ek_achha,sirf_root_achha});
-        return max(koi_ek_achha,sirf_root_achha);
+        maxSum=max({maxSum,niche_hi_mil_gaya,koi_ek_achha,sirf_root_achha});
+        return max({koi_ek_achha,sirf_root_achha});
     }
     int maxPathSum(TreeNode* root) {
          solve(root);
