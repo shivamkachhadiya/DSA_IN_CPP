0401434306c157e01109dd5b36bb80ac44037203
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-18 19:34:54 +0530

    Time: 6 ms (44.83%), Space: 23.9 MB (59.56%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& arr) {
-        int n=arr.size();
         sort(arr.begin(),arr.end());
         vector<vector<int>>ans;
-        for(int i=0;i<n;i++){
-            if(ans.empty()||ans.back()[1]<arr[i][0]){
+        for(int i=0;i<arr.size();i++){
+            if(ans.empty()){
                 ans.push_back(arr[i]);
-            }else{
-                ans.back()[1]=max(ans.back()[1],arr[i][1]);
+                continue;
+            }
+            else{
+                int take_first_element=arr[i][0];
+                int take_second_element=arr[i][1];
+                int take_last_ans_element=ans.back()[1];
+                if(take_first_element<=take_last_ans_element){
+                    ans.back()[1]=max(take_last_ans_element,take_second_element);
+                }else{
+                    ans.push_back(arr[i]);
+                }
             }
         }
         return ans;
