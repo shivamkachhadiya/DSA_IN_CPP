da868167baf6d9d98ebb8b8e9033fa62c0fd5756
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-18 14:58:56 +0530

    Time: 182 ms (93.7%), Space: 10.3 MB (93.03%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    bool solve(int n,int m,vector<vector<char>>&mat,int i,int j,string word,int idx){
-        if(idx==word.size())return true;
-        if(i<0||j<0||i>=n||j>=m||mat[i][j]=='$')return false;
-        if(mat[i][j]!=word[idx])return false;
-        char temp=mat[i][j];
-        mat[i][j]='$';
-//{
-        //furthr call up left right down
-        if(solve(n,m,mat,i+1,j,word,idx+1))return true;
-        if(solve(n,m,mat,i,j+1,word,idx+1))return true;
-        if(solve(n,m,mat,i-1,j,word,idx+1))return true;
-        if(solve(n,m,mat,i,j-1,word,idx+1))return true;
-//}
-        mat[i][j]=temp;
-        return false;
+    bool solve(vector<vector<char>>& mat, int n, int m, string& word, int i,
+               int j, int idx) {
+        int size = word.size();
+
+        if (idx >= size)
+            return true;
+        if (i < 0 || j < 0 || i >= n || j >= m)
+            return false;
+
+        if (mat[i][j] != word[idx])
+            return false;
+        char temp = mat[i][j];
+        mat[i][j] = '$';
+
+        bool result=solve(mat, n, m, word, i + 1, j, idx + 1)||
+                    solve(mat, n, m, word, i, j + 1, idx + 1)||
+                    solve(mat, n, m, word, i - 1, j, idx + 1)||
+                    solve(mat, n, m, word, i, j - 1, idx + 1);
+
+        mat[i][j] = temp;
+        return result;
     }
     bool exist(vector<vector<char>>& mat, string word) {
-        int n=mat.size();
-        int m=mat[0].size();
+        int n = mat.size();
+        int m = mat[0].size();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(mat[i][j]==word[0]&&solve(n,m,mat,i,j,word,0)){
-                    return true;
-                }
+                if(solve(mat,n,m,word,i,j,0))return true;
             }
         }
         return false;
