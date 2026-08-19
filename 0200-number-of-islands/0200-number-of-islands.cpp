c9f9bf0363358cd513fdb999c076bd858c4582da
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-19 19:01:40 +0530

    Time: 28 ms (48.44%), Space: 16.7 MB (61.63%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    int delR[4]={-1,1,0,0};
-    int delC[4]={0,0,-1,1};
-    void dfs(vector<vector<char>>&grid,int n,int m,vector<vector<bool>>&vis,int i,int j){
+    int delRow[4]={-1,1,0,0};
+    int delCol[4]={0,0,-1,1};
+    void dfs(vector<vector<char>>&grid,vector<vector<bool>>&vis,int i,int j,int n,int m){
         vis[i][j]=true;
-
-        for(int x=0;x<4;x++){
-            int newRow=i+delR[x];
-            int newCol=j+delC[x];
-            if(newRow>=0&&newCol>=0&&newRow<n&&newCol<m&&vis[newRow][newCol]==false&&grid[newRow][newCol]=='1'){
-                dfs(grid,n,m,vis,newRow,newCol);
+        
+        for(int k=0;k<4;k++){
+            int newRow=i+delRow[k];
+            int newCol=j+delCol[k];
+            if(newRow>=0&&newCol>=0&&newRow<n&&newCol<m&&grid[newRow][newCol]=='1'&&vis[newRow][newCol]==false){
+                dfs(grid,vis,newRow,newCol,n,m);
             }
         }
     }
     int numIslands(vector<vector<char>>& grid) {
-        int islands=0;
         int n=grid.size();
         int m=grid[0].size();
+        int islands=0;
         vector<vector<bool>>vis(n,vector<bool>(m,false));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(vis[i][j]==false&&grid[i][j]!='0'){
-                    dfs(grid,n,m,vis,i,j);
+                if(vis[i][j]==0&&grid[i][j]=='1'){
+                    dfs(grid,vis,i,j,n,m);
                     islands++;
                 }
             }
