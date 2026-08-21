f8192c137b294715a89d54120c5c946196e2bd0a
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-21 18:30:23 +0530

    Time: 60 ms (49.3%), Space: 21.3 MB (41.48%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,41 +1,30 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord,
-                     vector<string>& wordList) {
-
-        unordered_set<string> st(wordList.begin(), wordList.end());
-
-        if (st.find(endWord) == st.end())
-            return 0;
-
-        queue<pair<string, int>> q;
-        q.push({beginWord, 1});
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        queue<pair<string,int>>q;
+        q.push({beginWord,1});
+        unordered_set<string>st(wordList.begin(),wordList.end());
         st.erase(beginWord);
 
-        while (!q.empty()) {
-            string word = q.front().first;
-            int len = q.front().second;
+        while(!q.empty()){
+            string word=q.front().first;
+            int steps=q.front().second;
             q.pop();
-
-            if (word == endWord)
-                return len;
-
-            for (int i = 0; i < word.size(); i++) {
-                char original = word[i];
-
-                for (char ch = 'a'; ch <= 'z'; ch++) {
-                    word[i] = ch;
-
-                    if (st.find(word) != st.end()) {
+            if(word==endWord)return steps;
+            for(int i=0;i<word.size();i++){
+                char original=word[i];
+                for(char ch='a';ch<='z';ch++){
+                    word[i]=ch;
+                    //is exist in set
+                    if(st.find(word)!=st.end()){
                         st.erase(word);
-                        q.push({word, len + 1});
+                        q.push({word,steps+1});
                     }
                 }
-
-                word[i] = original;
+                word[i]=original;
+            
             }
         }
-
         return 0;
     }
 };
