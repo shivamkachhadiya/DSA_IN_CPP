161b122b0585f4e87c75507033502b0406bbeaaa
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-21 16:57:23 +0530

    Time: 0 ms (100%), Space: 17.2 MB (16.55%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,44 +3,47 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        queue<pair<pair<int,int>,int>>q;
-        vector<vector<int>>vis(n,vector<int>(m,false));
+        vector<vector<bool>>vis(n,vector<bool>(m,false));
+        queue<pair<int,pair<int,int>>>q;
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
                     vis[i][j]=true;
-                    q.push({{i,j},0});
+                    q.push({0,{i,j}});
                 }
             }
         }
-        int delRow[]={-1,1,0,0,};
-        int delCol[]={0,0,1,-1};
+
+        int delRow[]={-1,1,0,0};
+        int delCol[]={0,0,-1,1};
         int ans=0;
         while(!q.empty()){
-            int row=q.front().first.first;
-            int col=q.front().first.second;
-            int time=q.front().second;
+            int time=q.front().first;
+            int row=q.front().second.first;
+            int col=q.front().second.second;
+
             q.pop();
-            ans=time;
+            ans=max(ans,time);
+
             for(int k=0;k<4;k++){
                 int newRow=row+delRow[k];
                 int newCol=col+delCol[k];
-                if(newRow>=0&&newCol>=0&&newRow<n&&newCol<m&&
-                    vis[newRow][newCol]==false&&grid[newRow][newCol]==1){
+                if(newRow>=0&&newCol>=0&&newRow<n&&newCol<m&&vis[newRow][newCol]==false&&grid[newRow][newCol]==1){
                     vis[newRow][newCol]=true;
-                    grid[newRow][newCol]=2;
-                    q.push({{newRow,newCol},time+1});
-                    
+                    q.push({time+1,{newRow,newCol}});
                 }
             }
         }
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
+                if(grid[i][j]==1&&vis[i][j]==false){
                     return -1;
                 }
             }
         }
         return ans;
+
     }
 };
