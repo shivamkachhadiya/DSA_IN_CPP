769d8983f6352ae29d7f54f4d60001a3bb96fc44
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-23 16:06:20 +0530

    Time: 0 ms (100%), Space: 9.8 MB (8.24%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    unordered_map<char, string> map_={
-        {'2',"abc"},
+    unordered_map<char,string>ask_map{
+     {'2',"abc"},
         {'3',"def"},
         {'4',"ghi"},
         {'5',"jkl"},
@@ -10,25 +10,24 @@ public:
         {'8',"tuv"},
         {'9',"wxyz"}
     };
-    vector<string> ans;
+    vector<string>ans;
 
-    void solve(string digits, int n, int i, string temp) {
-        if (i >= n) {
-            ans.push_back(temp);
+    void solve(string digits,string& part,int idx){
+        if(idx==digits.size()){
+            ans.push_back(part);
             return;
         }
-
-        string str = map_[digits[i]];
-        for (int idx = 0; idx < str.size(); idx++) {
-            temp.push_back(str[idx]);
-            solve(digits, n, i + 1, temp);
-            temp.pop_back();
+        string curr_string=ask_map[digits[idx]];
+      
+        for(int i=0;i<curr_string.size();i++){
+            part.push_back(curr_string[i]);
+            solve(digits,part,idx+1);
+            part.pop_back();
         }
     }
-
     vector<string> letterCombinations(string digits) {
-        int n = digits.length();
-        solve(digits, n, 0, "");
+        string part="";
+        solve(digits,part,0);
         return ans;
     }
 };
