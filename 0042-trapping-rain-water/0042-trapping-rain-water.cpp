d9786fa2fbe0d828ec3f2d63e7f10c9a660d8ed4
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-25 19:07:23 +0530

    Time: 0 ms (100%), Space: 26.1 MB (56.46%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    int trap(vector<int>& arr) {
-        int n = arr.size();
-        int left = 0;
-        int right = n - 1;
-        int ans = 0;
-        int left_max = INT_MIN;
-        int right_max = INT_MIN;
-        while (left < right) {
-            left_max = max(left_max, arr[left]);
-            right_max = max(right_max, arr[right]);
-
-            // deciding factor
-            if (left_max < right_max) {
-                // left decide
-                int trapped_water = left_max - arr[left];
-                ans += trapped_water;
+    int trap(vector<int>& height) {
+        int n=height.size();
+        int left=0;
+        int right=n-1;
+        int left_max=INT_MIN;
+        int right_max=INT_MIN;
+        int ans=0;
+        while(left<right){
+            left_max=max(left_max,height[left]);
+            right_max=max(right_max,height[right]);
+            if(left_max<right_max){
+                int trap_water=left_max-height[left];
+                ans+=trap_water;
                 left++;
-            } else {
-                // right decide
-                int trapped_water = right_max - arr[right];
-                ans += trapped_water;
+            }else{
+                int trap_water=right_max-height[right];
+                ans+=trap_water;
                 right--;
             }
         }
