a5c205d8f37b11405beaa852843245e4966bd19e
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-26 22:12:25 +0530

    Time: 74 ms (36.33%), Space: 82.4 MB (45.83%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    void solve(int n,int k,vector<vector<int>>&ans,vector<int>&part,int i){
+    vector<vector<int>>ans;
+    void solve(vector<int>&arr,int n,int k,vector<int>&part,int i){
         if(k==0){
             ans.push_back(part);
             return;
         }
-        if(i>n){
-            return;
-        }
-
-        for(int start=i;start<=n;start++){
-            part.push_back(start);
-            solve(n,k-1,ans,part,start+1);
-            part.pop_back();
-        }
-
-        
+        if(i>=n)return;
+        part.push_back(arr[i]);
+        solve(arr,n,k-1,part,i+1);
+        part.pop_back();
+        solve(arr,n,k,part,i+1);
     }
     vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>>ans;
+        vector<int>arr;
+        for(int i=1;i<=n;i++){
+            arr.push_back(i);
+        }
         vector<int>part;
-        solve(n,k,ans,part,1);
+        solve(arr,n,k,part,0);
         return ans;
     }
 };
