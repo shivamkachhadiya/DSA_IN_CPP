343e4e29e7adcc0baff9545b716822763c8a8108
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-27 17:05:58 +0530

    Time: 0 ms (100%), Space: 14.2 MB (46.52%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-vector<vector<int>>ans;
-    void solve(vector<int>&arr,int target,vector<int>&part,int n,int i,int currsum){
-        if(i==n){
-            if(currsum==target){
+    vector<vector<int>> ans;
+    void solve(vector<int>& arr, int target, int i, vector<int>& part,
+               int sum) {
+        if (i >= arr.size()) {
+            if (sum == target) {
                 ans.push_back(part);
                 return;
             }
             return;
         }
-        if(currsum>target)return;
-        if(currsum==target){
+        if (sum == target) {
             ans.push_back(part);
             return;
         }
-       
+
+        if(sum>target)return;
+
         part.push_back(arr[i]);
-        solve(arr,target,part,n,i,currsum+arr[i]);
+        solve(arr,target,i,part,sum+arr[i]);
         part.pop_back();
-        solve(arr,target,part,n,i+1,currsum);
+        solve(arr,target,i+1,part,sum);
     }
     vector<vector<int>> combinationSum(vector<int>& arr, int target) {
-        vector<int>part;
-        int n=arr.size();
-        solve(arr,target,part,n,0,0);
+        int n = arr.size();
+        vector<int> part;
+        solve(arr, target, 0, part, 0);
         return ans;
     }
 };
