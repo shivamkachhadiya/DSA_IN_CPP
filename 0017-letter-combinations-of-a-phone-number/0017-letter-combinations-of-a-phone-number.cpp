67124ca542d946558b14b4121e596236755c9c21
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-27 16:47:42 +0530

    Time: 0 ms (100%), Space: 9.6 MB (19.43%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     unordered_map<char,string>ask_map{
-     {'2',"abc"},
-        {'3',"def"},
-        {'4',"ghi"},
-        {'5',"jkl"},
-        {'6',"mno"},
-        {'7',"pqrs"},
-        {'8',"tuv"},
-        {'9',"wxyz"}
+        {'2',"abc"},{'3',"def"},{'4',"ghi"},{'5',"jkl"},{'6',"mno"},{'7',"pqrs"},
+        {'8',"tuv"},{'9',"wxyz"}
     };
     vector<string>ans;
-
-    void solve(string digits,string& part,int idx){
+    
+    void solve(string &digits,string &part,int idx){
         if(idx==digits.size()){
             ans.push_back(part);
             return;
         }
         string curr_string=ask_map[digits[idx]];
-      
         for(int i=0;i<curr_string.size();i++){
             part.push_back(curr_string[i]);
             solve(digits,part,idx+1);
@@ -28,6 +21,6 @@ public:
     vector<string> letterCombinations(string digits) {
         string part="";
         solve(digits,part,0);
-        return ans;
+        return ans;    
     }
 };
