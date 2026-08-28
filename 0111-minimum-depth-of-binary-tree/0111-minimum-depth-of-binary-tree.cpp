e06da4b53c0c1fa70566fec431d6061261f38525
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-28 21:10:55 +0530

    Time: 1 ms (62.78%), Space: 146.7 MB (91.77%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -1,14 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int minDepth(TreeNode* root) {
-        if (!root) return 0; // agar empty tree hai
-        
-        // agar left null hai to right ka depth lena hoga
-        if (!root->left) return 1 + minDepth(root->right);
-        // agar right null hai to left ka depth lena hoga
-        if (!root->right) return 1 + minDepth(root->left);
-        
-        // dono child present hain
-        return 1 + min(minDepth(root->left), minDepth(root->right));
+    int solve(TreeNode* root) {
+        if (root == NULL)
+            return 0;
+        if (root->left == NULL && root->right == NULL)
+            return 1;
+        if (root->left == NULL && root->right != NULL)
+            return 1+solve(root->right);
+        if (root->left != NULL && root->right == NULL)
+            return 1+solve(root->left);
+        return 1 + min(solve(root->left), solve(root->right));
     }
+    int minDepth(TreeNode* root) { return solve(root); }
 };
