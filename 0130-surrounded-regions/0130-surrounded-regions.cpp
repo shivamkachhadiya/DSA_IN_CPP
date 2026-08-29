a5fc4125924fd85d89459649b936281b1cdf41a6
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-29 16:38:37 +0530

    Time: 0 ms (100%), Space: 14.3 MB (68.32%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,58 +1,36 @@
 class Solution {
-private:
-    void dfs(int row, int col, vector<vector<int>>& vis,
-             vector<vector<char>>& mat, int deltaRow[], int deltaCol[]) {
-        // visit mark whichever you perfom dfs
-        vis[row][col] = 1;
-        int n = mat.size();
-        int m = mat[0].size();
-
-        // after visiting check top right down left
-        for (int i = 0; i < 4; i++) {
-            int newRow = row + deltaRow[i];
-            int newCol = col + deltaCol[i];
-            if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m &&
-                !vis[newRow][newCol] && mat[newRow][newCol] == 'O') {
-                dfs(newRow, newCol, vis, mat, deltaRow, deltaCol);
+public:
+    int delRow[4]={-1,1,0,0};
+    int delCol[4]={0,0,-1,1};
+    void dfs(vector<vector<char>>&grid,int n,int m,vector<vector<bool>>&vis,int i,int j){
+        vis[i][j]=true;
+        for(int k=0;k<4;k++){
+            int newRow=delRow[k]+i;
+            int newCol=j+delCol[k];
+            if(newRow<n&&newCol<m&&newRow>=0&&newCol>=0&&vis[newRow][newCol]==false&&grid[newRow][newCol]=='O'){
+                //grid[newRow][newCol]='X';
+                dfs(grid,n,m,vis,newRow,newCol);
             }
         }
     }
+    void solve(vector<vector<char>>& board) {
+        int n=board.size();
+        int m=board[0].size();
+        vector<vector<bool>>vis(n,vector<bool>(m,false));
 
-public:
-    void solve(vector<vector<char>>& mat) {
-        int deltaRow[] = {-1, +0, +1, +0};
-        int deltaCol[] = {+0, +1, +0, -1};
-
-        int n = mat.size();
-        int m = mat[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-
-        // boundry visit
-        // traverse first row and last row
-        for (int j = 0; j < m; j++) {
-            if (!vis[0][j] && mat[0][j] == 'O') {
-                dfs(0, j, vis, mat, deltaRow, deltaCol);
-            }
-            if (!vis[n - 1][j] && mat[n - 1][j] == 'O') {
-                dfs(n - 1, j, vis, mat, deltaRow, deltaCol);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if((i==0||j==0||i==n-1||j==m-1)&&board[i][j]=='O'&&vis[i][j]==false){
+                    dfs(board,n,m,vis,i,j);
+                }
             }
         }
 
-        // first column and last coloumn
-        for (int i = 0; i < n; i++) {
-            if (!vis[i][0] && mat[i][0] == 'O') {
-                dfs(i, 0, vis, mat, deltaRow, deltaCol);
-            }
-            if (!vis[i][m - 1] && mat[i][m - 1] == 'O') {
-                dfs(i, m - 1, vis, mat, deltaRow, deltaCol);
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!vis[i][j] && mat[i][j] == 'O')
-                    // no touch make its 1
-                    mat[i][j] = 'X';
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(vis[i][j]==false&&board[i][j]=='O'){
+                    board[i][j] = 'X';
+                }
             }
         }
     }
