8587fac4f7033815e5963ab9aa9982f6496964ee
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-29 18:17:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
-    int solve(int n,int i,vector<int>&dp){
-        if(i>n)return 0;
+    int solve(int n,int i){
         if(i==n)return 1;
-        if(dp[i]!=-1)return dp[i];
-        return dp[i]=solve(n,i+1,dp)+solve(n,i+2,dp);
+        if(i>n)return 0;
+        return solve(n,i+1)+solve(n,i+2);
     }
     int climbStairs(int n) {
-        vector<int>dp(n,-1);
-        return solve(n,0,dp);
+        return solve(n,0);
     }
 };
