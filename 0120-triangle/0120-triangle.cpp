88624adc195ac6c0d6419397d1304783c97f6656
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-30 15:27:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-
-        // Create a 2D dp table of size n x n, initialized to 0
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-
-        // Base case: last row of dp = last row of triangle
-        for (int j = 0; j < n; j++) {
-            dp[n - 1][j] = triangle[n - 1][j];
+    int solve(vector<vector<int>>& mat, int n, int m, int i, int j,
+              vector<vector<int>>& dp) {
+        if (i == n - 1) {
+            return mat[i][j];
         }
-
-        // Bottom-up: fill dp from second last row up to the top
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = 0; j <= i; j++) {
-                // Option 1: go down
-                int down = triangle[i][j] + dp[i + 1][j];
-                // Option 2: go diagonal
-                int diagonal = triangle[i][j] + dp[i + 1][j + 1];
-                // Take minimum
-                dp[i][j] = min(down, diagonal);
-            }
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        return dp[i][j] = mat[i][j] + min(solve(mat, n, m, i + 1, j, dp),
+                                          solve(mat, n, m, i + 1, j + 1, dp));
+    }
+    int minimumTotal(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>> dp(n, vector<int>());
+        for (int i = 0; i < n; i++) {
+            dp[i] = vector<int>(mat[i].size(), -1);
         }
-
-        // Answer will be stored at the top of dp
-        return dp[0][0];
+        return solve(mat, n, m, 0, 0, dp);
     }
 };
