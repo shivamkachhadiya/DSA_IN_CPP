330a451123cb2cd53e127063c3512b5bd5e7fca9
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-08-30 15:35:23 +0530

    Time: 0 ms (100%), Space: 13.4 MB (25.79%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -5,17 +5,20 @@ public:
         if (i == n - 1) {
             return mat[i][j];
         }
-        if (dp[i][j] != -1)
+        if (dp[i][j] != INT_MAX)
             return dp[i][j];
-        return dp[i][j] = mat[i][j] + min(solve(mat, n, m, i + 1, j, dp),
-                                          solve(mat, n, m, i + 1, j + 1, dp));
+
+        int downans=solve(mat, n, m, i + 1, j, dp);
+        int diagonalans=solve(mat, n, m, i + 1, j + 1, dp);
+        int finalans=mat[i][j] + min(downans,diagonalans);
+        return dp[i][j] = finalans;
     }
     int minimumTotal(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
         vector<vector<int>> dp(n, vector<int>());
         for (int i = 0; i < n; i++) {
-            dp[i] = vector<int>(mat[i].size(), -1);
+            dp[i] = vector<int>(mat[i].size(), INT_MAX);
         }
         return solve(mat, n, m, 0, 0, dp);
     }
