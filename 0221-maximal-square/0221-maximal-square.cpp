71472db576b90d71190a4c69981e43c5245f337b
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-09-04 16:03:21 +0530

    Time: 34 ms (12.75%), Space: 31.1 MB (18.28%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
-        int solve(vector<vector<char>>& mat, int i, int j, int &maxi, vector<vector<int>>& dp) {
-
-        //base case cross boundry
-        if(i>=mat.size()||j>=mat[0].size()){
+    int solve(vector<vector<char>>& mat, int i, int j, int& maxi,vector<vector<int>>&dp) {
+        if (i >= mat.size() || j >= mat[0].size()) {
             return 0;
         }
+        if(dp[i][j]!=-1)return dp[i][j];
+        int right = solve(mat, i, j + 1, maxi,dp);
+        int bottom = solve(mat, i + 1, j, maxi,dp);
+        int diagonal = solve(mat, i + 1, j + 1, maxi,dp);
 
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-
-        int right=solve(mat,i,j+1,maxi,dp);
-        int diagonal=solve(mat,i+1,j+1,maxi,dp);
-        int down=solve(mat,i+1,j,maxi,dp);
-
-        if(mat[i][j]=='1'){
-            dp[i][j]=1+min(right,min(diagonal,down));
-            maxi=max(maxi,dp[i][j]);
-            return dp[i][j];
-        }else{
+        if (mat[i][j] == '1') {
+            int ans = 1 + min({right, bottom, diagonal});
+            maxi = max(ans, maxi);
+            return dp[i][j]=ans;
+        } else {
             return dp[i][j]=0;
         }
     }
-    int maximalSquare(vector<vector<char>>& mat) {
-        int maxi=0;
-        int n=mat.size();
-        int m=mat[0].size();
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int maxi = 0;
+        int n=matrix.size();
+        int m=matrix[0].size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        solve(mat,0,0,maxi,dp);
+        solve(matrix, 0, 0, maxi,dp);
         return maxi * maxi;
     }
 };
