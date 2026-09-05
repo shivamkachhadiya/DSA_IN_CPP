a2bcc669148bb09f5ba760ac190261d79da49620
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-09-05 18:25:27 +0530

    Time: 7 ms (46.29%), Space: 19.3 MB (68.61%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
-    bool solve(int src,vector<bool>&vis,vector<bool>recPath,vector<vector<int>>&adj){
-        vis[src]=true;
-        recPath[src]=true;
-        for(auto &x:adj[src]){
-            if(vis[x]==false){
-                if(solve(x,vis,recPath,adj)){
+    bool solve(int n,vector<bool>&vis,vector<vector<int>>&arr,vector<vector<int>>&adj,int start,vector<bool>&isRecPath){
+        vis[start]=true;
+        isRecPath[start]=true;
+        for(auto &p:adj[start]){
+            if(vis[p]==false){
+                if(solve(n,vis,arr,adj,p,isRecPath)){
                     return true;
                 }
-            }else if(recPath[x]==true){
+            }else if(isRecPath[p]==true){
                 return true;
             }
         }
-        recPath[src]=false;
+        isRecPath[start]=false;
         return false;
     }
-    bool canFinish(int n, vector<vector<int>>& edges) {
-        vector<bool>vis(n,false);
-        vector<bool>recPath(n,false);
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
         vector<vector<int>>adj(n+1);
-        for(int i=0;i<edges.size();i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
-            adj[u].push_back(v);
+        for(auto &x:prerequisites){
+            int u=x[0];
+            int v=x[1];
+            adj[v].push_back(u);
         }
+        vector<bool>vis(n,false);
+        vector<bool>isRecPath(n,false);
         for(int i=0;i<n;i++){
             if(vis[i]==false){
-                if(solve(i,vis,recPath,adj)){
+                if(solve(n,vis,prerequisites,adj,i,isRecPath)){
                     return false;
                 }
             }
         }
         return true;
-
     }
 };
