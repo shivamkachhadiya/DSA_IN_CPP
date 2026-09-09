b34721d7cd9688d7db49af8009902062229dd56b
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-09-09 19:03:02 +0530

    Time: 4 ms (12.88%), Space: 10.9 MB (23.24%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    void solve(vector<int>&nums,int n,vector<vector<int>>&ans,vector<int>&part,int i){
-        if(i>=n){
-            ans.push_back(part);
+    vector<vector<int>> ans;
+     set<vector<int>> st;
+    void solve(vector<int>& arr, int n, vector<int>& part, int i) {
+        if (i >= n) {
+           st.insert(part); 
             return;
         }
-        part.push_back(nums[i]);
-        solve(nums,n,ans,part,i+1);
+        part.push_back(arr[i]);
+        solve(arr, n, part, i + 1);
         part.pop_back();
-        int new_idx=i+1;
-        while(new_idx<n&&nums[new_idx]==nums[new_idx-1]){
-            new_idx++;
-        }
-        solve(nums,n,ans,part,new_idx);
+        solve(arr, n, part, i + 1);
     }
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>ans;
-        vector<int>part;
-        sort(nums.begin(), nums.end());
-        solve(nums,n,ans,part,0);
-        return ans;
+    vector<vector<int>> subsetsWithDup(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int n = arr.size();
+        vector<int> part;
+        solve(arr, n, part, 0);
+        return vector<vector<int>>(st.begin(),st.end());
     }
 };
