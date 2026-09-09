3c722c66893b035fad3248169c8ebc73eed0eae5
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-09-09 15:13:52 +0530

    Time: 0 ms (100%), Space: 10.4 MB (7.56%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    int solve(vector<int>& arr, int target, int sum, vector<int>& dp) {
-        if(sum == target) return 1;   // ek valid way mila
-        if(sum > target) return 0;    // target cross ho gaya
-
-        if(dp[sum] != -1) return dp[sum];  // memoization check
-
-        int ans = 0;
-        for(int i = 0; i < arr.size(); i++) {
-            ans += solve(arr, target, sum + arr[i], dp);
+    int solve(vector<int>&arr,int target,int i,int n,vector<vector<int>>&dp){
+        if(target==0)return 1;
+        if(target<0)return 0;
+        if(i>=n){
+            if(target==0){
+                return 1;
+            }
+            return 0;
         }
-        return dp[sum] = ans;
-    }
+        if(dp[i][target]!=-1)return dp[i][target];
+        int take=solve(arr,target-arr[i],0,n,dp);
+        int notake=solve(arr,target,i+1,n,dp);
 
-    int combinationSum4(vector<int>& nums, int target) {
-        vector<int> dp(target + 1, -1);  // memo table
-        return solve(nums, target, 0, dp);
+        return dp[i][target]=take+notake;
+    }
+    int combinationSum4(vector<int>& arr, int target) {
+        int n=arr.size();
+        vector<vector<int>>dp(n,vector<int>(target+1,-1));
+        return solve(arr,target,0,arr.size(),dp);
     }
 };
