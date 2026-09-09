47444696f62832c2670ef9ac5d7853341f13c071
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-09-09 11:28:47 +0530

    Time: 0 ms (100%), Space: 69.7 MB (38.53%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -5,15 +5,19 @@ public:
         int left=0;
         int count=0;
         int r=0;
+
+        int maxlen=0;
         for(;r<n;r++){
             if(arr[r]==0){
                 count++;
             }
-            if(count>k){
-                if(arr[left]==0)count--;
+            while(count>k){
+                if(arr[left]==0)
+                    count--;
                 left++;
             }
+            maxlen=max(maxlen,r-left+1);
         }
-        return r-left;
+        return maxlen;
     }
 };
