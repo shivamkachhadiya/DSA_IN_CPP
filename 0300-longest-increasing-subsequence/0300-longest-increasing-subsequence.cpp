0b10f6bea4de9554756d6e0b084ea3a135b4c545
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-09-09 15:33:23 +0530

    Time: 646 ms (5.04%), Space: 296.6 MB (6.99%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,58 +1,20 @@
-// class Solution {
-// public:
-//     vector<int>ans;
-//     int size=0;
-//     void solve(vector<int>&arr,int n,int i){
-//         if(i>=n){
-//             size=max(size,(int)ans.size());
-//             return;
-//         }
-
-//         if(ans.empty()||ans.back()<arr[i]){
-//             //include
-//             ans.push_back(arr[i]);
-//             solve(arr,n,i+1);
-//             ans.pop_back();            
-//         }
-//         solve(arr,n,i+1);
-
-//     }
-//     int lengthOfLIS(vector<int>& nums) {
-//         int n=nums.size();
-//         solve(nums,n,0);
-//         return size;
-//     }
-// };
 class Solution {
 public:
-    int solve(vector<int>& nums, int i, int prev,
-              vector<vector<int>>& dp) {
-
-        if (i == nums.size())
+    int solve(vector<int>& arr, int n, int i, int p, vector<vector<int>>& dp) {
+        if (i >= arr.size())
             return 0;
-
-        // prev ranges from -1 to n-1
-        if (dp[i][prev + 1] != -1)
-            return dp[i][prev + 1];
-
-        // Include current element
+        if (dp[i][p+1] != -1)
+            return dp[i][p+1];
         int take = 0;
-
-        if (prev == -1 || nums[i] > nums[prev]) {
-            take = 1 + solve(nums, i + 1, i, dp);
+        if (p == -1 || arr[p] < arr[i]) {
+            take += 1 + solve(arr, n, i + 1, i, dp);
         }
-        
-        // Exclude current element
-        int notTake = solve(nums, i + 1, prev, dp);
-
-        return dp[i][prev + 1] = max(take, notTake);
+        int notake = solve(arr, n, i + 1, p, dp);
+        return dp[i][p+1] = max(take, notake);
     }
-
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-
+    int lengthOfLIS(vector<int>& arr) {
+        int n = arr.size();
         vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-
-        return solve(nums, 0, -1, dp);
+        return solve(arr, n, 0, -1, dp);
     }
 };
