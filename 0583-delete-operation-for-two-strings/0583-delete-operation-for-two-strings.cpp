ab1cf9cdd32ac0cf7797c5bbdea54dcf459d8e4a
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-09-10 16:36:08 +0530

    Time: 649 ms (5%), Space: 376.7 MB (5.03%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    int solve(string word1, string word2, int n1, int n2, int i, int j,vector<vector<int>>&dp) {
-        if (i >= n1 || j >= n2)
-            return 0;
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
+    int solve(string a,string b,int i,int j,vector<vector<int>>&dp){
+        if(i>=a.size()||j>=b.size())return 0;
+        
+        if(dp[i][j]!=-1)return dp[i][j];
+        int ans=0;
 
-        int pick = 0;
-        if (word1[i] == word2[j]) {
-            pick = 1 + solve(word1, word2, n1, n2, i + 1, j + 1,dp);
+        if(a[i]==b[j]){
+            ans=1+solve(a,b,i+1,j+1,dp);
+        }else{
+            ans=max(solve(a,b,i+1,j,dp),solve(a,b,i,j+1,dp));
         }
-        int nonpick = max(solve(word1, word2, n1, n2, i + 1, j,dp),
-                          solve(word1, word2, n1, n2, i, j + 1,dp));
-        return dp[i][j]=max(pick, nonpick);
+        return dp[i][j]=ans;
     }
     int minDistance(string word1, string word2) {
-        int n1 = word1.size();
-        int n2 = word2.size();
-        vector<vector<int>>dp(n1,vector<int>(n2,-1));
-        return word1.size()+word2.size()-2*solve(word1, word2, n1, n2, 0, 0,dp);
-
+        vector<vector<int>>dp(word1.size(),vector<int>(word2.size(),-1));
+        int finslcs=solve(word1,word2,0,0,dp);
+        return word1.size()-finslcs+word2.size()-finslcs;
     }
 };
