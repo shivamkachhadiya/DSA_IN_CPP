b091a1f4d528235b6378bec70ad39ace30389dda
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-09-10 11:31:50 +0530

    Time: 11 ms (39.9%), Space: 11.2 MB (75.11%) - LeetHub

---
+++ 1115-print-foobar-alternately/1115-print-foobar-alternately.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class FooBar {
+private:
+    int n;
+    mutex m;
+    condition_variable cv;
+    bool fooTurn = true;
+
+public:
+    FooBar(int n) { this->n = n; }
+    void foo(function<void()> printFoo) {
+
+        for (int i = 0; i < n; i++) {
+
+            unique_lock<mutex> lock(m);
+
+            while (!fooTurn) {
+                cv.wait(lock);
+            }
+
+            printFoo();
+
+            fooTurn = false;
+
+            cv.notify_all();
+        }
+    }
+    void bar(function<void()> printBar) {
+
+        for (int i = 0; i < n; i++) {
+
+            unique_lock<mutex> lock(m);
+
+            while (fooTurn) {
+                cv.wait(lock);
+            }
+
+            printBar();
+
+            fooTurn = true;
+
+            cv.notify_all();
+        }
+    }
+};
