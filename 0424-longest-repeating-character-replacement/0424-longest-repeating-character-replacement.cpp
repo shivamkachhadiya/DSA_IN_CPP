31217907a19d2fe019dd824d18854cef5265176f
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-09-11 22:01:16 +0530

    Time: 8 ms (36.61%), Space: 11 MB (19.1%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        vector<int> freq(26, 0); // to store frequency of characters
-        int left = 0, maxFreq = 0, maxLen = 0;
+        unordered_map<int, int> map;
+        int left = 0;
+        int count = 0;
+        int n = s.size();
+        int maxF = 0;
 
-        for (int right = 0; right < s.size(); right++) {
-            freq[s[right] - 'A']++;
-            maxFreq = max(maxFreq, freq[s[right] - 'A']);
+        for (int right = 0; right < n; right++) {
+            map[s[right] - 'A']++;
+            maxF = max(maxF, map[s[right] - 'A']);
 
-            // Total letters to change = window size - maxFreq
-            if ((right - left + 1) - maxFreq > k) {
-                freq[s[left] - 'A']--;
+            while ((right-left+1)-maxF>k) {
+                map[s[left] - 'A']--;
                 left++;
             }
 
-            maxLen = max(maxLen, right - left + 1);
+            count = max(count, right - left + 1);
         }
-
-        return maxLen;
+        return count;
     }
 };
