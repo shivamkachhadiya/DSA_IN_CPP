35a064c66ba775937d1c053cec25029395b1ca6e
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-09-11 15:27:36 +0530

    Time: 29 ms (6.03%), Space: 21.5 MB (6.92%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,60 +1,23 @@
 class Solution {
 public:
-    bool solve(int start, string s, unordered_set<string>& st) {
+    int n;
+    bool solve(int idx, string &s, unordered_set<string>& st, vector<int>& dp) {
+        if(idx == n) return true;
+        if(dp[idx] != -1) return dp[idx];  // already computed
 
-        // whole string consumed
-        if (start == s.size())
-            return true;
-
-        string temp = "";
-
-        for (int i = start; i < s.size(); i++) {
-
-            temp += s[i];
-
-            // word found in dictionary
-            if (st.find(temp) != st.end()) {
-
-                // check remaining part
-                if (solve(i + 1, s, st))
-                    return true;
+        for(int len = 1; len <= n; len++) {
+            string temp = s.substr(idx, len);
+            if(st.find(temp) != st.end() && solve(idx + len, s, st, dp)) {
+                return dp[idx] = true;
             }
         }
-
-        return false;
+        return dp[idx] = false;
     }
 
-    bool solveMemo(int start, string s, unordered_set<string>& st,vector<int>& dp) {
-
-        // whole string consumed
-        if (start == s.size())
-            return true;
-
-        if(dp[start]!=-1)return dp[start];
-
-        string temp = "";
-
-        for (int i = start; i < s.size(); i++) {
-
-            temp += s[i];
-
-            // word found in dictionary
-            if (st.find(temp) != st.end()) {
-
-                // check remaining part
-                if (solveMemo(i + 1, s, st, dp))
-                    return dp[start]=true;
-            }
-        }
-
-        return dp[start]=false;
-    }
     bool wordBreak(string s, vector<string>& wordDict) {
-
         unordered_set<string> st(wordDict.begin(), wordDict.end());
-
-        vector<int> dp(s.size(), -1);
-
-        return solveMemo(0, s, st,dp);
+        n = s.size();
+        vector<int> dp(n, -1);  // -1 = not computed
+        return solve(0, s, st, dp);
     }
 };
