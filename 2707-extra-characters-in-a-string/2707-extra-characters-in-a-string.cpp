ca3abdbaed35b19885cf0836f1f0bea36762af1f
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-09-11 17:12:05 +0530

    Time: 171 ms (18.11%), Space: 111.8 MB (27.67%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    int solve(string s,vector<string>&arr,unordered_set<string>&st,int i,int n,vector<int>&dp){
-        if(i==n)return 0;
-        if(dp[i]!=-1)return dp[i];
-        int no_extra=1e9;
-        for(int j=1;i+j<=n;j++){
-            string temp=s.substr(i,j);
-            if(st.find(temp)!=st.end()){
-                no_extra=min(no_extra,solve(s,arr,st,i+j,n,dp));
+    int solve(string s, vector<string>& arr, unordered_set<string>& st, int i,
+              int n, vector<int>& dp) {
+        if (i == n)
+            return 0;
+        if (dp[i] != -1)
+            return dp[i];
+        int no_extra = 1e9;
+        for (int j = 1; j+i<=n; j++) {
+            string temp = s.substr(i, j);
+            if (st.find(temp) != st.end()) {
+                no_extra = min(no_extra, solve(s, arr, st, i + j, n, dp));
             }
         }
-        int take_extra=1+solve(s,arr,st,i+1,n,dp);
-        return dp[i]=min(no_extra,take_extra);
+        int take_extra = 1 + solve(s, arr, st, i + 1, n, dp);
+        return dp[i] = min(no_extra, take_extra);
     }
     int minExtraChar(string s, vector<string>& arr) {
-        unordered_set<string>st(arr.begin(),arr.end());
-        vector<int>dp(s.size(),-1);
-        return solve(s,arr,st,0,s.size(),dp);
+        unordered_set<string> st(arr.begin(), arr.end());
+        vector<int> dp(s.size(), -1);
+        return solve(s, arr, st, 0, s.size(), dp);
     }
 };
