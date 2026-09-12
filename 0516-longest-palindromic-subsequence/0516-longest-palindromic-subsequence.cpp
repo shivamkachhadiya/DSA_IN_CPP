843293a6e960fdbab59470325135dc30f5db6ef6
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-09-12 12:06:14 +0530

    Time: 295 ms (5.17%), Space: 76.2 MB (8.48%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    int solve(string s,string temp,int i,int j,vector<vector<int>>&dp){
-        if(i==s.size()||j==temp.size()) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        int take=0;
-        if(s[i]==temp[j]){
-            take=1+solve(s,temp,i+1,j+1,dp);
+    int solve(string &s, string &temp, int i, int j, vector<vector<int>>& dp) {
+        if (i == s.size() || j == temp.size())
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int take = 0;
+        if (s[i] == temp[j]) {
+            take = 1 + solve(s, temp, i + 1, j + 1, dp);
         }
-        int notake=max(solve(s,temp,i+1,j,dp),solve(s,temp,i,j+1,dp));
-        
-        return dp[i][j]=max(take,notake);
+        int notake = max(solve(s, temp, i + 1, j, dp),
+                         solve(s, temp, i, j + 1, dp));
+
+        return dp[i][j] = max(take, notake);
     }
-    
+
     int longestPalindromeSubseq(string s) {
-        string temp=s;
-        reverse(temp.begin(),temp.end());
-        int n=s.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1,-1));  // 👈 fix size
-        return solve(s,temp,0,0,dp);
+        string temp = s;
+        reverse(temp.begin(), temp.end());
+        int n = s.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));  //  dynamic size
+        return solve(s, temp, 0, 0, dp);
     }
 };
