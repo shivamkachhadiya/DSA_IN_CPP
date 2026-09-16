dc26f5c157d7577f3580018bb4808e01b5878eed
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-09-16 21:12:36 +0530

    Time: 0 ms (100%), Space: 17.8 MB (30.07%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int solve(vector<int>& arr, int n, int i,vector<int>&dp) {
-        if (i < 0) {
-            return 0;
-        }
-        if (i == 0 || i == 1)
-            return arr[i];
+    int solve(vector<int>&arr,int n,int i,vector<int>&dp){
+        if(i==n)return 0;
+        if(i>n)return 0;
         if(dp[i]!=-1)return dp[i];
-        return dp[i]=arr[i] + min(solve(arr, n, i - 1,dp), solve(arr, n, i - 2,dp));
+        int oneStep=arr[i]+solve(arr,n,i+1,dp);
+        int twoStep=arr[i]+solve(arr,n,i+2,dp);
+
+        return dp[i]=min(oneStep,twoStep);
+        
     }
-    int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int>dp(n+1,-1);
-        return min(solve(cost,n, n-1,dp), solve(cost, n,n-2,dp));
+    int minCostClimbingStairs(vector<int>& arr) {
+        vector<int>dp(arr.size()+1,-1);
+        return min(solve(arr,arr.size(),0,dp),solve(arr,arr.size(),1,dp));
     }
 };
