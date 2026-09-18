91a75e140c45c00c57d89e132eee7e7322203ed0
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-09-18 16:14:25 +0530

    Time: 76 ms (80.67%), Space: 44.3 MB (70.66%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int, int>>> adj(n+1);
-        int size = times.size();
-        for (int i = 0; i < size; i++) {
-            int source = times[i][0];
-            int destination = times[i][1];
-            int time = times[i][2];
-            adj[source].push_back({destination, time});
+        vector<vector<pair<int,int>>>adj(n+1);
+        for(int i=0;i<times.size();i++){
+            int u=times[i][0];
+            int v=times[i][1];
+            int nodeDistance=times[i][2];
+            adj[u].push_back({v,nodeDistance});
         }
-        vector<int> dist_vec(n+1, 1e9);
-        dist_vec[k] = 0;
-        priority_queue<pair<int, int>, vector<pair<int, int>>,
-                       greater<pair<int, int>>>
-            pq;
-        pq.push({0, k});
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        //distance node
+        pq.push({0,k});
 
-        while (!pq.empty()) {
-            int dist = pq.top().first;
-            int node = pq.top().second;
+        vector<int>dist_vec(n+1,1e9);
+        dist_vec[k]=0;
+        while(!pq.empty()){
+            int dist=pq.top().first;
+            int Node=pq.top().second;
             pq.pop();
-            for (auto& x : adj[node]) {
-                int newNode = x.first;
-                int newDist = dist + x.second;
-               
-                if (newDist < dist_vec[newNode]) {
-                    dist_vec[newNode] = newDist;
-                    pq.push({newDist, newNode});
+             if(dist > dist_vec[Node]) continue; 
+            for(auto &x:adj[Node]){
+                int nextNodeDistance=x.second;
+                int nextNode=x.first;
+                if(dist_vec[nextNode]>nextNodeDistance+dist){
+                    dist_vec[nextNode]=nextNodeDistance+dist;
+                    pq.push({nextNodeDistance+dist,nextNode});
                 }
             }
         }
-        int ans=*max_element(dist_vec.begin()+1,dist_vec.end());
-        if(ans==1e9)return -1;
+        int ans=0;
+        for(int i=1;i<=n;i++){  
+            if(dist_vec[i]==1e9) return -1;
+            ans=max(ans,dist_vec[i]);
+        }
         return ans;
     }
 };
