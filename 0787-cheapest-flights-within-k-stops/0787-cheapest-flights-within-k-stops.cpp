949bacd61704d98fb9af122493f423190df755ca
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-09-19 18:25:50 +0530

    Time: 4 ms (48.16%), Space: 17.9 MB (66.3%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,42 +1,49 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
+    int findCheapestPrice(int n, vector<vector<int>>& arr, int src, int dst,
                           int k) {
-
+        // int n=arr.size();
+        // int m=arr[0].size();
+        const int INF = 1e9;
         vector<vector<pair<int, int>>> adj(n);
-        for (int i = 0; i < flights.size(); i++) {
-            int source = flights[i][0];
-            int dest = flights[i][1];
-            int cost = flights[i][2];
-
-            adj[source].push_back({dest, cost});
+        // adj
+        for (int i = 0; i < arr.size(); i++) {
+            int u = arr[i][0];
+            int v = arr[i][1];
+            int distance = arr[i][2];
+            adj[u].push_back({v, distance});
         }
+        queue<pair<int, pair<int, int>>> pq;
 
-        queue<pair<int, pair<int, int>>> q;
-        q.push({0, {src, 0}});
-        vector<int> dest_arr(n, 1e9);
-        dest_arr[src]=0;
-        while (!q.empty()) {
-            auto it = q.front();
-            q.pop();
-            int stop = it.first;
-            int node = it.second.first;
-            int cost = it.second.second;
+        // pq= {stops,node,distance}
+        pq.push({0, {src, 0}});
+        vector<int> distArr(n, INF);
+        distArr[src] = 0;
 
-            for (auto x : adj[node]) {
+        while (!pq.empty()) {
+            auto it = pq.front();
+            pq.pop();
+            int Stops = it.first;
+            int Node = it.second.first;
+            int Distance = it.second.second;
+
+            // if (Stops > k) continue;
+            // if(Node==dst)return Distance;
+
+            for (auto& x : adj[Node]) {
                 int newNode = x.first;
-                int newCost = cost + x.second;
-                int newStop = stop + 1;
-                if (newStop <= k+1) {
-                    if (newCost < dest_arr[newNode]) {
-                        dest_arr[newNode] = newCost;
-                        q.push({newStop, {newNode, newCost}});
+                int newCost = Distance + x.second;
+                int newStop = Stops + 1;
+                if (newStop <= k + 1) {
+                    if (newCost < distArr[newNode]) {
+                        distArr[newNode] = newCost;
+                        pq.push({newStop, {newNode, newCost}});
                     }
                 }
             }
         }
-        if (dest_arr[dst] != 1e9)
-            return dest_arr[dst];
+        if (distArr[dst] != 1e9)
+            return distArr[dst];
         return -1;
     }
 };
