15a83811bdcc10c45f5fa52760da4ed5c887e45d
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-09-19 11:35:23 +0530

    Time: 51 ms (65.39%), Space: 24.5 MB (77.43%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,39 +1,46 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 1e9));
-        vis[0][0] = 0;
-        priority_queue<pair<int, pair<int, int>>,
-                       vector<pair<int, pair<int, int>>>,
-                       greater<pair<int, pair<int, int>>>>
-            pq;
-        pq.push({0, {0, 0}});
-        int delRow[] = {-1, 1, 0, 0};
-        int delCol[] = {0, 0, -1, 1};
-        while (!pq.empty()) {
-            int distance = pq.top().first;
-            int i = pq.top().second.first;
-            int j = pq.top().second.second;
-            pq.pop();            
-            if (i == n-1 && j == m-1) return distance; 
-            for (int k = 0; k < 4; k++) {
-                int newRow = i + delRow[k];
-                int newCol = j + delCol[k];
-                if (newRow >= 0 && newCol >= 0 && newRow < n && newCol < m) {
-                    int jaha_se_aaye=mat[i][j];
-                    int currNode = mat[newRow][newCol];
+        int n=mat.size();
+        int m=mat[0].size();
+        
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
 
-                    int diff = abs(currNode - jaha_se_aaye);
-                    int newEffort=max(distance,diff);
-                    if (newEffort < vis[newRow][newCol]) {
-                        vis[newRow][newCol] = newEffort;
-                        pq.push({newEffort,{ newRow, newCol}});
+       
+        vector<vector<int>>dist(n,vector<int>(m,1e9));
+
+        dist[0][0]=0;
+
+        pq.push({0,{0,0}});
+
+        int dr[]={-1,1,0,0};
+        int dc[]={0,0,-1,1};
+        while(!pq.empty()){
+            auto it=pq.top();
+            pq.pop();
+
+
+            int diff=it.first;
+            int row=it.second.first;
+            int col=it.second.second;
+
+            if(row==n-1&&col==m-1)return diff;
+
+            for(int i=0;i<4;i++){
+                int newr=row+dr[i];
+                int newc=col+dc[i];
+
+                if(newr>=0&&newc>=0&&newr<n&&newc<m){
+                    int newEff=max(abs(mat[row][col]-mat[newr][newc]),diff);
+                    if(newEff<dist[newr][newc]){
+                        dist[newr][newc]=newEff;
+                        pq.push({newEff,{newr,newc}});
                     }
                 }
             }
         }
-        return vis[n-1][m-1];
+
+        return 0;
+    
     }
 };
