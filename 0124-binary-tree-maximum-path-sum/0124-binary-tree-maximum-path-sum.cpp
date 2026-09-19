2a8603d4653cd06a0bff76250998388a4850ad7a
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-09-19 22:04:28 +0530

    Time: 0 ms (100%), Space: 28.1 MB (22.12%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -6,27 +6,33 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int solve(TreeNode*root,int &maxi){
-        if(root==NULL)return 0;
-    
-        int left_ans=solve(root->left,maxi);
-        int right_ans=solve(root->right,maxi);
+    int ans=INT_MIN;
+    int solve(TreeNode* root) {
+        if (root == NULL)
+            return 0;
+        
+        int left=solve(root->left);
+        int right=solve(root->right);
 
-        int case1=root->val+left_ans+right_ans;
-        int case2=root->val+max(left_ans,right_ans);
-        int case3=root->val;
-
-        maxi=max({maxi,case1,case2,case3});
-        return max(case2,case3);
+        int leftOnlyTakeWithReturn = root->val + left;
+        int rightOnlyTakeWithReturn = root->val + right;
+        int takeBothWithNoReturn =
+            root->val + left +right;
+        int takeOnlyRootWithReturn = root->val;
+        int currSum = max({leftOnlyTakeWithReturn, rightOnlyTakeWithReturn,
+                          takeBothWithNoReturn, takeOnlyRootWithReturn});
+        ans=max(ans,currSum);
+        return max({leftOnlyTakeWithReturn, rightOnlyTakeWithReturn,
+                    takeOnlyRootWithReturn});
     }
     int maxPathSum(TreeNode* root) {
-        int maxi=INT_MIN;
-        solve(root,maxi);
-        return maxi;
+         solve(root);
+        return ans;
     }
 };
