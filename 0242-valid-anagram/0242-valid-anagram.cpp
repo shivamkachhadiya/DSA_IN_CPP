b529489c9e33bfaafad1a83172fcfdfa3d76baad
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-09-21 12:10:57 +0530

    Time: 8 ms (6.69%), Space: 9.9 MB (25.26%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int>freq1;
-        unordered_map<char,int>freq2;
+        unordered_map<char,int>map;
+        for(int i=0;i<s.size();i++){
+            map[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++){
+            if(map.find(t[i])!=map.end()){
+                map[t[i]]--;
+                if(map[t[i]]==0){
+                    map.erase(t[i]);
+                }
+            }
+            else return false;
+        }
+        return map.empty();
         
-        int si=s.size();
-        int n=t.size();
-        if(si!=n)return false;
-
-
-        for(auto ch:s){
-            freq1[ch]++;
-        }
-         for(auto ch:t){
-            freq2[ch]++;
-        }
-     
-        if(freq1==freq2)return true;
-        return false;
     }
 };
